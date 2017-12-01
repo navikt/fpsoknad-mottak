0cbaf842278bf132ed236a4841abd7e092cb63bc
janolaveide <janolaveide@gmail.com>
2017-12-01 14:57:03 +0100

    create test that always fails
    To test slack notifications

---
+++ src/test/foreldrepenger/Test.java
@@ -0,0 +1,14 @@
+package no.nav.foreldrepenger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestDummy {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
