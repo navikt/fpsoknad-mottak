6e7323d8b38897752d434c6c76d1d51ebc917097
Jan-Olav Eide <janolaveide@gmail.com>
2017-12-04 16:31:28 +0100

    renamed main

--- service/src/main/java/no/nav/foreldrepenger/selvbetjening/OppstartsApplication.java
+++ service/src/main/java/no/nav/foreldrepenger/selvbetjening/OppstartsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  HelloApusApplication {
+public class  OppstartsApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(HelloApusApplication.class, args);
+    SpringApplication.run(OppstartsApplication.class, args);
   }
 }
