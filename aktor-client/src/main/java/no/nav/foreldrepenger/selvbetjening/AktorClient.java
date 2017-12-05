1ae9e87715865c4c1a32cb30d71648d2847647fa
Jan-Olav Eide <janolaveide@gmail.com>
2017-12-05 13:31:56 +0100

    moved common stuff to parent pom

--- aktor-client/src/main/java/no/nav/foreldrepenger/selvbetjening/AktorClient.java
+++ aktor-client/src/main/java/no/nav/foreldrepenger/selvbetjening/AktorClient.java
@@ -1,20 +1,43 @@
 package no.nav.foreldrepenger.selvbetjening;
 
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
 import no.nav.tjeneste.virksomhet.aktoer.v2.HentAktoerIdForIdentResponse;
-import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.*;
-import org.springframework.ws.client.core.support.*;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentAktoerIdForIdentRequest;
 
-public class AktorClient extends WebServiceGatewaySupport {
+public class AktorClient  implements AktorOperations {
+   
+   private final WebServiceTemplate template;
+   
+   public AktorClient(@Value("${integration.aktoridservice}") String uri) {
+      this(templateFrom(uri));
+   }
 
-    public HentAktoerIdForIdentResponse aktorIdForFnr(String fnr) {
+    private AktorClient(WebServiceTemplate template) {
+      this.template = Objects.requireNonNull(template);
+   }
 
+  
+   private static WebServiceTemplate templateFrom(String uri) {
+      WebServiceTemplate template = new WebServiceTemplate();
+      template.setDefaultUri(uri);
+      return template;
+   }
+
+   
+   @Override
+   public String aktorIdForFnr(String fnr) {
         HentAktoerIdForIdentRequest request = new HentAktoerIdForIdentRequest();
         request.setIdent(fnr);
-
-        HentAktoerIdForIdentResponse response = (HentAktoerIdForIdentResponse) getWebServiceTemplate()
-                .marshalSendAndReceive("http://something", request);
-
-        return response;
+        HentAktoerIdForIdentResponse response = (HentAktoerIdForIdentResponse) template.marshalSendAndReceive(request);
+        return response.getHentAktoerIdForIdentResponse().getAktoerId();
     }
-
+   
+   @Override
+   public String toString() {
+      return getClass().getSimpleName() + " [template=" + template + "]";
+   }
 }
---
+++ aktor-client/src/main/java/no/nav/foreldrepenger/selvbetjening/AktorOperations.java
@@ -0,0 +1,7 @@
+package no.nav.foreldrepenger.selvbetjening;
+
+public interface AktorOperations {
+
+   String aktorIdForFnr(String fnr);
+
+}
