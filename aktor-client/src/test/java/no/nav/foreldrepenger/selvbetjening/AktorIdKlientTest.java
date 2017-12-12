22fbdb4d51465374a1105de82a822c8c760cef25
Jan-Olav Eide <janolaveide@gmail.com>
2017-12-12 14:58:56 +0100

    dummy test

--- aktor-client/src/test/java/no/nav/foreldrepenger/selvbetjening/AktorIdKlientTest.java
+++ aktor-client/src/test/java/no/nav/foreldrepenger/selvbetjening/AktorIdKlientTest.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import javax.inject.Inject;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,18 +15,18 @@ import no.nav.foreldrepenger.selvbetjening.aktorklient.domain.Fodselsnummer;
 import no.nav.modig.testcertificates.TestCertificates;
 
 	
-	
+	@Ignore
 	@RunWith(SpringRunner.class)
 	@SpringBootTest
 	public class AktorIdKlientTest {
 
 
-	    
+	       
 
 	        @Inject
 	        private AktorIdKlient client;
 
-	     //@Test
+	     @Test
 	    public void useSts() throws Exception {
 	          TestCertificates.setupKeyAndTrustStore();
 	       Optional<AktorId> aktorId = client.aktorIdForFnr(new Fodselsnummer("06055301296"));
