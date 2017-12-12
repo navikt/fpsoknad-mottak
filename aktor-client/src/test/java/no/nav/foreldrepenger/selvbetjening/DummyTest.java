d8b04dbba5f3a7b2338ca33a5f5bda56e9ea238b
Jan-Olav Eide <janolaveide@gmail.com>
2017-12-12 14:56:18 +0100

    dummy test

---
+++ aktor-client/src/test/java/no/nav/foreldrepenger/selvbetjening/DummyTest.java
@@ -0,0 +1,13 @@
+package no.nav.foreldrepenger.selvbetjening;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void dummy() {
+		assertTrue(true);
+	}
+}
