2de572935def0aed4317a48b4c31523d41293c8e
Jan-Olav Eide <janolaveide@gmail.com>
2017-12-13 09:31:22 +0100

    Added swagger, moved root to /startup

--- service/src/main/java/no/nav/foreldrepenger/selvbetjening/IndexController.java
+++ service/src/main/java/no/nav/foreldrepenger/selvbetjening/IndexController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/startup")
 public class IndexController {
 
    @Inject
