47af892e65ac252a87a5b4131480888015d15233
Jan-Olav Eide <janolaveide@gmail.com>
2018-01-04 14:55:41 +0100

    more stuff from TPS

--- foreldrepenger-selvbetjening-service/src/main/java/no/nav/foreldrepenger/selvbetjening/controllers/StartupController.java
+++ foreldrepenger-selvbetjening-service/src/main/java/no/nav/foreldrepenger/selvbetjening/controllers/StartupController.java
@@ -5,13 +5,9 @@ import java.util.Optional;
 import javax.inject.Inject;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-<<<<<<< HEAD
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-=======
-import no.nav.foreldrepenger.selvbetjening.person.klient.PersonKlient;
->>>>>>> more stuff from TPS
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -26,6 +22,7 @@ import no.nav.foreldrepenger.selvbetjening.domain.AktorId;
 import no.nav.foreldrepenger.selvbetjening.domain.Fodselsnummer;
 import no.nav.foreldrepenger.selvbetjening.domain.ID;
 import no.nav.foreldrepenger.selvbetjening.domain.Person;
+import no.nav.foreldrepenger.selvbetjening.person.klient.PersonKlient;
 
 @RestController
 @Validated
@@ -34,37 +31,26 @@ public class StartupController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(StartupController.class);
 
-   @Inject
-   private AktorIdKlient aktorClient;
-   @Inject
-   private PersonKlient personClient;
+	@Inject
+	private AktorIdKlient aktorClient;
+	@Inject
+	private PersonKlient personClient;
 
-
-   @GetMapping(value = "/")
-   public ResponseEntity<Person> startup(@Valid @RequestParam(value="fnr", required=true) Fodselsnummer fnr) {
-	  Optional<AktorId> aktorId = aktorClient.aktorIdForFnr(fnr);
-	  Person person = personClient.hentPersonInfo(new ID(aktorId.get(),fnr));
-	  return new ResponseEntity<Person>(person, HttpStatus.OK);
-   }
-
-   @ExceptionHandler({ConstraintViolationException.class})
-   public ResponseEntity<String> handleValidationException(ConstraintViolationException e) {
-       return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-   }
-
-<<<<<<< HEAD
-
-   @Override
-	public String toString() {
-		return getClass().getSimpleName() + " [aktorClient=" + aktorClient + "]";
+	@GetMapping(value = "/")
+	public ResponseEntity<Person> startup(@Valid @RequestParam(value = "fnr", required = true) Fodselsnummer fnr) {
+		Optional<AktorId> aktorId = aktorClient.aktorIdForFnr(fnr);
+		Person person = personClient.hentPersonInfo(new ID(aktorId.get(), fnr));
+		return new ResponseEntity<Person>(person, HttpStatus.OK);
 	}
-=======
-@Override
-public String toString() {
-	return getClass().getSimpleName() + " [aktorClient=" + aktorClient + ", personClient=" + personClient + "]";
-}
-  
-   
->>>>>>> more stuff from TPS
+
+	@ExceptionHandler({ ConstraintViolationException.class })
+	public ResponseEntity<String> handleValidationException(ConstraintViolationException e) {
+		return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " [aktorClient=" + aktorClient + ", personClient=" + personClient + "]";
+	}
 
 }
