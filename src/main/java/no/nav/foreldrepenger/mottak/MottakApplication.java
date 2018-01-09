53d44cf97ec7793491b8d6874aaf712d310ec92c
Jan-Olav Eide <janolaveide@gmail.com>
2018-01-09 13:16:59 +0100

    first go man program

---
+++ src/main/java/no/nav/foreldrepenger/mottak/MottakApplication.java
@@ -0,0 +1,30 @@
+package no.nav.foreldrepenger.mottak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+
+import no.nav.modig.testcertificates.TestCertificates;
+
+@SpringBootApplication
+@ComponentScan("no.nav.foreldrepenger.oppslag")
+public class MottakApplication {
+
+	public static void main(String[] args) {
+		setProperty("no.nav.modig.security.sts.url", System.getenv("SECURITYTOKENSERVICE_URL"));
+		setProperty("no.nav.modig.security.systemuser.username", System.getenv("FPSELVBETJENING_USERNAME"));
+		setProperty("no.nav.modig.security.systemuser.password", System.getenv("FPSELVBETJENING_PASSWORD"));
+		TestCertificates.setupKeyAndTrustStore();
+		SpringApplication.run(MottakApplication.class, args);
+	}
+
+	private static void setProperty(String key, String value) {
+		if (value == null) {
+			throw new IllegalArgumentException("Key " + key + " har ingen verdi");
+		}
+		System.setProperty(key, value);
+	}
+
+}
