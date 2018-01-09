b34820ce83df320cd6f13044910265db5c19a082
Jan-Olav Eide <janolaveide@gmail.com>
2018-01-09 12:44:50 +0100

    test dummy

---
+++ src/test/java/no/nav/foreldrepenger/oppslag/TestDummy.java
@@ -0,0 +1,17 @@
+package no.nav.foreldrepenger.oppslag;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestDummy {
+
+	
+	@Test
+	public void testDummy()  {
+		assertTrue(true);
+		
+	}
+}
