f3f6b430757e7ee9a94687ffb9b602159468ba28
Jan-Olav Eide <janolaveide@gmail.com>
2018-01-16 13:14:33 +0100

    exception handling for income lookup

---
+++ src/main/java/no/nav/foreldrepenger/oppslag/domain/exceptions/IncompleteRequestException.java
@@ -0,0 +1,13 @@
+package no.nav.foreldrepenger.oppslag.domain.exceptions;
+
+public class IncompleteRequestException extends RuntimeException {
+
+	public IncompleteRequestException(Throwable cause) {
+		this(null,cause);
+	}
+	
+	public IncompleteRequestException(String msg,Throwable cause) {
+		super(msg,cause);
+	}
+
+}
