1ab274f13bff6b732a7fb2b350f5412de44a4915
Jan-Olav Eide <janolaveide@gmail.com>
2018-02-15 13:23:03 +0100

    disable ping now pings dokmot queue

--- src/main/java/no/nav/foreldrepenger/mottak/http/MottakController.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/MottakController.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.mottak.http;
 
+import javax.inject.Inject;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -8,27 +9,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import no.nav.foreldrepenger.mottak.dokmot.JmsDokmotSender;
+import no.nav.foreldrepenger.mottak.dokmot.XMLEnvelopeGenerator;
 import no.nav.foreldrepenger.mottak.domain.Søknad;
 
 @RestController
 class MottakController {
 
-    // private final XMLEnvelopeGenerator dokmotXmlGenerator;
+    private final XMLEnvelopeGenerator dokmotXmlGenerator;
 
-    /* @Inject public MottakController(XMLEnvelopeGenerator xmlGenerator) { this.dokmotXmlGenerator = xmlGenerator; } */
+    private final JmsDokmotSender sender;
+
+    @Inject
+    public MottakController(XMLEnvelopeGenerator xmlGenerator, JmsDokmotSender sender) {
+        this.dokmotXmlGenerator = xmlGenerator;
+        this.sender = sender;
+    }
 
     @PostMapping(value = "/mottak", produces = { "application/xml" })
     public ResponseEntity<String> mottak(@Valid @RequestBody Søknad søknad) {
-        return ResponseEntity.status(HttpStatus.OK).body(/* dokmotXmlGenerator.getGenerator().toXML(søknad) */"OK");
+        return ResponseEntity.status(HttpStatus.OK).body(dokmotXmlGenerator.getGenerator().toXML(søknad));
     }
 
     @PostMapping(value = "/mottak/dokmot", produces = { "application/xml" })
     public ResponseEntity<String> mottakDokmot(@Valid @RequestBody Søknad søknad) {
-        return ResponseEntity.status(HttpStatus.OK).body(/* dokmotXmlGenerator.toXML(søknad) */"OK");
+        return ResponseEntity.status(HttpStatus.OK).body(dokmotXmlGenerator.toXML(søknad));
     }
 
     @PostMapping(value = "/mottak/ping", produces = { "application/xml" })
     public ResponseEntity<String> ping() {
+        sender.ping();
         return ResponseEntity.status(HttpStatus.OK).body("PONG");
     }
 
