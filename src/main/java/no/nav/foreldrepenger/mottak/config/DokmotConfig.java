ef3a883026efbed655ac7115d642eaaa5d599ddd
Jan-Olav Eide <janolaveide@gmail.com>
2018-02-15 12:04:03 +0100

    dokmot config back in

--- src/main/java/no/nav/foreldrepenger/mottak/config/DokmotConfig.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/DokmotConfig.java
@@ -10,6 +10,7 @@ import javax.jms.JMSException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jms.connection.UserCredentialsConnectionFactoryAdapter;
 import org.springframework.jms.core.JmsTemplate;
