3120236f7df445bcb2117d0725b4accc4e0b1dce
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-02-15 12:52:36 +0100

    component test for infotrygd

---
+++ src/test/java/no/nav/foreldrepenger/oppslag/infotrygd/InfotrygdEndpointTest.java
@@ -0,0 +1,49 @@
+package no.nav.foreldrepenger.oppslag.infotrygd;
+
+import no.nav.foreldrepenger.oppslag.domain.Ytelse;
+import no.nav.tjeneste.virksomhet.infotrygdsak.v1.binding.InfotrygdSakV1;
+import no.nav.tjeneste.virksomhet.infotrygdsak.v1.informasjon.InfotrygdSak;
+import no.nav.tjeneste.virksomhet.infotrygdsak.v1.meldinger.FinnSakListeRequest;
+import no.nav.tjeneste.virksomhet.infotrygdsak.v1.meldinger.FinnSakListeResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static no.nav.foreldrepenger.oppslag.infotrygd.TestdataProvider.sak;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Tag("slow")
+public class InfotrygdEndpointTest {
+
+   @MockBean
+   private InfotrygdSakV1 infotrygdSakV1;
+
+   @BeforeEach
+   public void setup() throws Exception {
+      FinnSakListeResponse response = new FinnSakListeResponse();
+      InfotrygdSak sak = sak();
+      response.getSakListe().add(sak);
+      when(infotrygdSakV1.finnSakListe(any(FinnSakListeRequest.class)))
+         .thenReturn(response);
+   }
+
+   @Autowired
+   private TestRestTemplate restTemplate;
+
+   @Test
+   public void makeHttpRequestAndDeserializeResult() {
+      Ytelse[] ytelser = restTemplate.getForObject("/infotrygd/?fnr=1234567890", Ytelse[].class);
+      assertEquals(1, ytelser.length);
+   }
+
+}
--- src/test/java/no/nav/foreldrepenger/oppslag/infotrygd/InfotrygdsakMapperTest.java
+++ src/test/java/no/nav/foreldrepenger/oppslag/infotrygd/InfotrygdsakMapperTest.java
@@ -2,15 +2,13 @@ package no.nav.foreldrepenger.oppslag.infotrygd;
 
 import no.nav.foreldrepenger.oppslag.domain.Ytelse;
 import no.nav.tjeneste.virksomhet.infotrygdsak.v1.informasjon.InfotrygdSak;
-import no.nav.tjeneste.virksomhet.infotrygdsak.v1.informasjon.Status;
-import no.nav.tjeneste.virksomhet.infotrygdsak.v1.informasjon.Tema;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.datatype.DatatypeFactory;
 import java.time.LocalDate;
 import java.util.Optional;
 
+import static no.nav.foreldrepenger.oppslag.infotrygd.TestdataProvider.sak;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InfotrygdsakMapperTest {
@@ -18,14 +16,7 @@ public class InfotrygdsakMapperTest {
     @Test
     @Tag("fast")
     public void mapValues() throws Exception {
-        InfotrygdSak sak = new InfotrygdSak();
-        sak.setVedtatt(DatatypeFactory.newInstance().newXMLGregorianCalendar("2017-12-13"));
-        Status status = new Status();
-        status.setTermnavn("statusen");
-        sak.setStatus(status);
-        Tema tema = new Tema();
-        tema.setTermnavn("typen");
-        sak.setTema(tema);
+        InfotrygdSak sak = sak();
         LocalDate date = LocalDate.of(2017, 12, 13);
         Ytelse expected = new Ytelse("typen","statusen", date, Optional.empty());
         Ytelse actual = InfotrygdsakMapper.map(sak);
