060cc676292aedb84c72ad954bff0b493a2324c2
Jan-Olav Eide <janolaveide@gmail.com>
2018-03-05 11:35:40 +0100

    revert

--- src/main/java/no/nav/foreldrepenger/oppslag/fpsak/FpsakConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/fpsak/FpsakConfiguration.java
@@ -1,26 +1,27 @@
 package no.nav.foreldrepenger.oppslag.fpsak;
 
-import no.nav.foreldrepenger.oppslag.ws.WsClient;
-import no.nav.tjeneste.virksomhet.foreldrepengesak.v1.binding.ForeldrepengesakV1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+import no.nav.foreldrepenger.oppslag.ws.WsClient;
+import no.nav.tjeneste.virksomhet.foreldrepengesak.v1.binding.ForeldrepengesakV1;
+
 @Configuration
 public class FpsakConfiguration {
 
-   @Autowired
-   public FpsakConfiguration(Environment environment) {
-      System.setProperty("SECURITYTOKENSERVICE_URL", environment.getProperty("SECURITYTOKENSERVICE_URL"));
-      System.setProperty("FPSELVBETJENING_USERNAME", environment.getProperty("FPSELVBETJENING_USERNAME"));
-      System.setProperty("FPSELVBETJENING_PASSWORD", environment.getProperty("FPSELVBETJENING_PASSWORD"));
-   }
+    @Autowired
+    public FpsakConfiguration(Environment environment) {
+        System.setProperty("SECURITYTOKENSERVICE_URL", environment.getProperty("SECURITYTOKENSERVICE_URL"));
+        System.setProperty("FPSELVBETJENING_USERNAME", environment.getProperty("FPSELVBETJENING_USERNAME"));
+        System.setProperty("FPSELVBETJENING_PASSWORD", environment.getProperty("FPSELVBETJENING_PASSWORD"));
+    }
 
-   @SuppressWarnings("unchecked")
-   @Bean
-   public ForeldrepengesakV1 fpsakV1(@Value("${VIRKSOMHET_FORELDREPENGESAK_V1_ENDPOINTURL:}") String serviceUrl) {
-      return new WsClient<ForeldrepengesakV1>().createPort(serviceUrl, ForeldrepengesakV1.class);
-   }
+    @SuppressWarnings("unchecked")
+    @Bean
+    public ForeldrepengesakV1 fpsakV1(@Value("${VIRKSOMHET_FORELDREPENGESAK_V1_ENDPOINTURL:jalla}") String serviceUrl) {
+        return new WsClient<ForeldrepengesakV1>().createPort(serviceUrl, ForeldrepengesakV1.class);
+    }
 }
