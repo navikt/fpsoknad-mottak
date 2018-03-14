66f8ec7ffa1b8006be3806ded9ac82eaa3c73fa0
Jan-Olav Eide <janolaveide@gmail.com>
2018-03-14 09:11:36 +0100

    added releses repo

--- src/main/java/no/nav/foreldrepenger/mottak/domain/Søknad.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/Søknad.java
@@ -2,7 +2,6 @@ package no.nav.foreldrepenger.mottak.domain;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,6 +9,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
@@ -24,37 +24,32 @@ public class Søknad {
     private final Ytelse ytelse;
     private String begrunnelseForSenSøknad;
     private String tilleggsopplysninger;
-    private final List<PåkrevdVedlegg> påkrevdeVedlegg;
-    private final List<ValgfrittVedlegg> frivilligeVedlegg;
+    private final List<Vedlegg> vedlegg;
 
     public Søknad(LocalDateTime mottattdato, Søker søker, Ytelse ytelse, Vedlegg... vedlegg) {
         this(mottattdato, søker, ytelse, Arrays.asList(vedlegg));
     }
 
-    public Søknad(LocalDateTime mottattdato, Søker søker, Ytelse ytelse, List<Vedlegg> vedlegg) {
-        this(mottattdato, søker, ytelse, påkrevde(vedlegg), valgfrie(vedlegg));
-    }
-
     @JsonCreator
     public Søknad(@JsonProperty("mottattdato") LocalDateTime mottattdato, @JsonProperty("søker") Søker søker,
-                  @JsonProperty("ytelse") Ytelse ytelse,
-                  @JsonProperty("påkrevdeVedlegg") List<PåkrevdVedlegg> påkrevdeVedlegg,
-                  @JsonProperty("frivilligeVedlegg") List<ValgfrittVedlegg> frivilligeVedlegg) {
+            @JsonProperty("ytelse") Ytelse ytelse,
+            @JsonProperty("påkrevdeVedlegg") List<Vedlegg> vedlegg) {
         this.mottattdato = mottattdato;
         this.søker = søker;
         this.ytelse = ytelse;
-        this.påkrevdeVedlegg = påkrevdeVedlegg == null ? Collections.emptyList() : påkrevdeVedlegg;
-        this.frivilligeVedlegg = frivilligeVedlegg == null ? Collections.emptyList() : frivilligeVedlegg;
+        this.vedlegg = vedlegg;
     }
 
-    private static List<PåkrevdVedlegg> påkrevde(List<Vedlegg> vedlegg) {
+    @JsonIgnore
+    public List<PåkrevdVedlegg> getPåkrevdeVedlegg() {
         return vedlegg.stream()
                 .filter(s -> s instanceof PåkrevdVedlegg)
                 .map(s -> PåkrevdVedlegg.class.cast(s))
                 .collect(Collectors.toList());
     }
 
-    private static List<ValgfrittVedlegg> valgfrie(List<Vedlegg> vedlegg) {
+    @JsonIgnore
+    public List<ValgfrittVedlegg> getFrivilligeVedlegg() {
         return vedlegg.stream()
                 .filter(s -> s instanceof ValgfrittVedlegg)
                 .map(s -> ValgfrittVedlegg.class.cast(s))
