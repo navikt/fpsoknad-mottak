e2919d7fb3ca4e4cfc6ee4509e187a20064370a8
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-03-14 14:28:52 +0100

    no need for ComponentScan annotation, Spring Boot does it anyway

--- src/test/java/no/nav/foreldrepenger/oppslag/OppslagApplicationLocal.java
+++ src/test/java/no/nav/foreldrepenger/oppslag/OppslagApplicationLocal.java
@@ -3,10 +3,8 @@ package no.nav.foreldrepenger.oppslag;
 import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 @EnableOIDCTokenValidation(ignore="org.springframework")
 public class OppslagApplicationLocal {
 
