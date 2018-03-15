0be1b460646b0d6b02b127b2f5fb95645be501ee
Jan-Olav Eide <janolaveide@gmail.com>
2018-03-15 13:43:39 +0100

    duplicate sub-types annotation removed

--- src/main/java/no/nav/foreldrepenger/mottak/config/JacksonModulesConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/JacksonModulesConfiguration.java
@@ -29,7 +29,7 @@ public class JacksonModulesConfiguration {
     }
 
     @Bean
-    public Module custonSerializers() {
+    public Module customSerializers() {
         return new CustomSerializerModule();
     }
 }
