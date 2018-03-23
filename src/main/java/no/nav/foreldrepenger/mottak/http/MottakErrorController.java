300c48454b9e28c796b20877cea580baef91fb19
Jan-Olav Eide <janolaveide@gmail.com>
2018-03-23 14:48:42 +0100

    first shot error controller

---
+++ src/main/java/no/nav/foreldrepenger/mottak/http/MottakErrorController.java
@@ -0,0 +1,21 @@
+package no.nav.foreldrepenger.mottak.http;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MottakErrorController implements ErrorController {
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Gikk galt, dette gitt.., har du logget på ?";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+}
