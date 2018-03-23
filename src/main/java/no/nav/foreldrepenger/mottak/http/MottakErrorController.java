2894944c282f82374f802f829506f22f0f2ad20b
Jan-Olav Eide <janolaveide@gmail.com>
2018-03-23 15:04:09 +0100

    error code

--- src/main/java/no/nav/foreldrepenger/mottak/http/MottakErrorController.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/MottakErrorController.java
@@ -1,5 +1,9 @@
 package no.nav.foreldrepenger.mottak.http;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +13,13 @@ import no.nav.security.spring.oidc.validation.api.Unprotected;
 @RestController
 @Unprotected
 public class MottakErrorController implements ErrorController {
+
+    @Autowired
     private static final String PATH = "/error";
 
     @RequestMapping(value = PATH)
-    public String error() {
-        return "Gikk galt, dette gitt.., har du logget på ?";
+    public String error(HttpServletRequest request, HttpServletResponse response) {
+        return "Gikk galt dette her, gitt. Responskode " + response.getStatus() + ", har du logget på ?";
     }
 
     @Override
