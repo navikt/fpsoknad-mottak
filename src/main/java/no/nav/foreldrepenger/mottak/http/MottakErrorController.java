963a269e5fe1951e08fef869f90894c9d23a6003
Jan-Olav Eide <janolaveide@gmail.com>
2018-03-23 14:52:45 +0100

    first shot error controller

--- src/main/java/no/nav/foreldrepenger/mottak/http/MottakErrorController.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/MottakErrorController.java
@@ -4,7 +4,10 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import no.nav.security.spring.oidc.validation.api.Unprotected;
+
 @RestController
+@Unprotected
 public class MottakErrorController implements ErrorController {
     private static final String PATH = "/error";
 
