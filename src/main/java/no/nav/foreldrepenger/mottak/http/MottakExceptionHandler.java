cac1a828b0bb15245c75f6516bb734efbf3466b8
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-04-03 09:45:50 +0200

    Add log when hendleMethodArgumentNotValid

--- src/main/java/no/nav/foreldrepenger/mottak/http/MottakExceptionHandler.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/MottakExceptionHandler.java
@@ -1,8 +1,10 @@
 package no.nav.foreldrepenger.mottak.http;
 
 import static java.util.stream.Collectors.joining;
+import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
+import org.slf4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +17,12 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class MottakExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger LOG = getLogger(MottakExceptionHandler.class);
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
+        LOG.error("Method argument not valid", e);
         return handleExceptionInternal(e, responseBody(e), new HttpHeaders(), UNPROCESSABLE_ENTITY, request);
     }
 
