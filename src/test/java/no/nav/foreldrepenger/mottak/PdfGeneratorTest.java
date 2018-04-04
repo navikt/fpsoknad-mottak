412d40871d734349ce18f906d5d1c0ac06476d2d
Jan-Olav Eide <janolaveide@gmail.com>
2018-04-04 10:41:03 +0200

    Update PdfGeneratorTest.java

--- src/test/java/no/nav/foreldrepenger/mottak/PdfGeneratorTest.java
+++ src/test/java/no/nav/foreldrepenger/mottak/PdfGeneratorTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import no.nav.foreldrepenger.mottak.config.MottakConfiguration;
-import no.nav.foreldrepenger.mottak.pdf.ByteArray2PdfConverter;
 import no.nav.foreldrepenger.mottak.pdf.PdfGenerator;
 
 @RunWith(SpringRunner.class)
@@ -23,10 +22,5 @@ public class PdfGeneratorTest {
     @Test
     public void signature() throws Exception {
         assertTrue(TestUtils.hasPdfSignature(gen.generate(TestUtils.engangssøknad(true))));
-    }
-
-    @Test
-    public void convert() throws Exception {
-        new ByteArray2PdfConverter().convert("pdf/jks.jpg");
     }
 }
