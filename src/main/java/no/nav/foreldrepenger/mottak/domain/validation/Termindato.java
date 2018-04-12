3ca2d3eea9b206a06a501347e1d1134e9394dea4
Jan-Olav Eide <janolaveide@gmail.com>
2018-04-12 14:39:40 +0200

    tekst for ukjent far var for lang, lagt til termindato annotering

---
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/validation/Termindato.java
@@ -0,0 +1,28 @@
+package no.nav.foreldrepenger.mottak.domain.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotNull;
+
+@NotNull
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = TermindatoValidator.class)
+@Documented
+@ReportAsSingleViolation
+public @interface Termindato {
+    String message() default "{ytelse.relasjontilbarn.framtidigfødsel.terminbekreftelse.fortid}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    int weeks() default 3;
+}
