5616884df13bbf1b8d42522948104e70ef2be77a
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-04-13 13:37:44 +0200

    disabling child lookup because tps doesn't allow it

--- src/main/java/no/nav/foreldrepenger/oppslag/person/PersonClientTpsWs.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/person/PersonClientTpsWs.java
@@ -6,6 +6,7 @@ import static no.nav.foreldrepenger.oppslag.person.RequestUtils.request;
 import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.ADRESSE;
 import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.FAMILIERELASJONER;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -53,10 +54,10 @@ public class PersonClientTpsWs implements PersonClient {
     public Person hentPersonInfo(ID id) {
 
         try {
-            HentPersonRequest request = request(id.getFnr(), ADRESSE, FAMILIERELASJONER);
+            HentPersonRequest request = request(id.getFnr(), ADRESSE);
             no.nav.tjeneste.virksomhet.person.v3.informasjon.Person tpsPerson =
                 hentPerson(id.getFnr(), request).getPerson();
-            Person person = PersonMapper.map(id, tpsPerson, barnFor(tpsPerson));
+            Person person = PersonMapper.map(id, tpsPerson, Collections.emptyList());
             collectLanguageMetrics(person);
             collectBankkontoMetrics(tpsPerson);
             return person;
