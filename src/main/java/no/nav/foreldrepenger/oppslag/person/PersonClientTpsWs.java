6b903f38ee29d900489b7dc540f0a2ac8a45aa82
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-04-25 14:27:21 +0200

    Removed child lookup. We don't need that info (yet), and it caused a bug when child had id type FDAT

--- src/main/java/no/nav/foreldrepenger/oppslag/person/PersonClientTpsWs.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/person/PersonClientTpsWs.java
@@ -1,21 +1,7 @@
 package no.nav.foreldrepenger.oppslag.person;
 
-import static no.nav.foreldrepenger.oppslag.person.RequestUtils.*;
-import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.ADRESSE;
-import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.BANKKONTO;
-import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.FAMILIERELASJONER;
-import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.KOMMUNIKASJON;
-
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
-import no.nav.foreldrepenger.oppslag.domain.Barn;
 import no.nav.foreldrepenger.oppslag.domain.Fodselsnummer;
 import no.nav.foreldrepenger.oppslag.domain.ID;
 import no.nav.foreldrepenger.oppslag.domain.Person;
@@ -25,12 +11,19 @@ import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonPersonIkkeFunnet;
 import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonSikkerhetsbegrensning;
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3;
 import no.nav.tjeneste.virksomhet.person.v3.informasjon.Bruker;
-import no.nav.tjeneste.virksomhet.person.v3.informasjon.Familierelasjon;
-import no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov;
-import no.nav.tjeneste.virksomhet.person.v3.informasjon.NorskIdent;
-import no.nav.tjeneste.virksomhet.person.v3.informasjon.PersonIdent;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonRequest;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.Objects;
+
+import static no.nav.foreldrepenger.oppslag.person.RequestUtils.request;
+import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.ADRESSE;
+import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.BANKKONTO;
+import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.KOMMUNIKASJON;
+import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.FAMILIERELASJONER;
 
 public class PersonClientTpsWs implements PersonClient {
 
@@ -60,7 +53,7 @@ public class PersonClientTpsWs implements PersonClient {
             HentPersonRequest request = request(id.getFnr(), ADRESSE, KOMMUNIKASJON, BANKKONTO, FAMILIERELASJONER);
             no.nav.tjeneste.virksomhet.person.v3.informasjon.Person tpsPerson = hentPerson(id.getFnr(), request)
                     .getPerson();
-            Person person = PersonMapper.map(id, tpsPerson, barnFor(tpsPerson));
+            Person person = PersonMapper.map(id, tpsPerson, Collections.emptyList());
             collectLanguageMetrics(person);
             collectBankkontoMetrics(tpsPerson);
             return person;
@@ -69,37 +62,6 @@ public class PersonClientTpsWs implements PersonClient {
             throw new RuntimeException(ex);
         }
 
-    }
-
-    private List<Barn> barnFor(no.nav.tjeneste.virksomhet.person.v3.informasjon.Person person) {
-        PersonIdent id = PersonIdent.class.cast(person.getAktoer());
-        String idType = id.getIdent().getType().getValue();
-        switch (idType) {
-        case FNR:
-        case DNR:
-            Fodselsnummer fnrMor = new Fodselsnummer(id.getIdent().getIdent());
-            return person.getHarFraRolleI().stream().filter(this::isBarn).map(s -> hentBarn(s, fnrMor))
-                    .filter(barn -> barneVelger.erStonadsberettigetBarn(fnrMor, barn)).collect(Collectors.toList());
-        default:
-            throw new IllegalStateException("ID type " + idType + " ikke støttet");
-        }
-    }
-
-    private boolean isBarn(Familierelasjon rel) {
-        return rel.getTilRolle().getValue().equals(BARN);
-    }
-
-    private Barn hentBarn(Familierelasjon rel, Fodselsnummer fnrMor) {
-        NorskIdent id = PersonIdent.class.cast(rel.getTilPerson().getAktoer()).getIdent();
-        if (RequestUtils.isFnr(id)) {
-            Fodselsnummer fnrBarn = new Fodselsnummer(id.getIdent());
-            return PersonMapper.map(id, fnrMor, hentPerson(fnrBarn, FAMILIERELASJONER));
-        }
-        throw new IllegalStateException("ID type " + id.getType().getValue() + " ikke støttet");
-    }
-
-    private HentPersonResponse hentPerson(Fodselsnummer fnr, Informasjonsbehov... behov) {
-        return hentPerson(fnr, request(fnr, behov));
     }
 
     private HentPersonResponse hentPerson(Fodselsnummer fnr, HentPersonRequest request) {
