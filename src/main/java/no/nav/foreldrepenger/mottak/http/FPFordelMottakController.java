fb9ac10efd0e46801c01fb676d6255c85a232bc3
Jan-Olav Eide <janolaveide@gmail.com>
2018-05-11 09:16:31 +0200

    unproted the whole thing for now

--- src/main/java/no/nav/foreldrepenger/mottak/http/FPFordelMottakController.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/FPFordelMottakController.java
@@ -18,12 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 import no.nav.foreldrepenger.mottak.domain.Kvittering;
 import no.nav.foreldrepenger.mottak.domain.Søknad;
 import no.nav.foreldrepenger.mottak.fpfordel.FPFordelSøknadSender;
-import no.nav.security.spring.oidc.validation.api.ProtectedWithClaims;
 import no.nav.security.spring.oidc.validation.api.Unprotected;
 
 @RestController
 @RequestMapping(path = FPFordelMottakController.FPFORDEL, produces = APPLICATION_JSON_VALUE)
-@ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
+// @ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
+@Unprotected
 public class FPFordelMottakController {
 
     private static final Logger LOG = getLogger(FPFordelMottakController.class);
@@ -37,7 +37,6 @@ public class FPFordelMottakController {
     }
 
     @GetMapping(value = "/ping")
-    @Unprotected
     public ResponseEntity<String> ping(@RequestParam(name = "navn", defaultValue = "earthling") String navn) {
         sender.ping();
         return ok("Hallo " + navn + " fra ubeskyttet ressurs");
