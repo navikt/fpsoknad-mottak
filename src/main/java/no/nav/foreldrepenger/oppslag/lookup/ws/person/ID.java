cdd3effc850454a9b605e63c290694ace0d2ac9d
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-05-29 10:25:47 +0200

    removed unnessecary use of the Pair class

--- src/main/java/no/nav/foreldrepenger/oppslag/lookup/ws/person/ID.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/lookup/ws/person/ID.java
@@ -1,49 +1,46 @@
 package no.nav.foreldrepenger.oppslag.lookup.ws.person;
 
-import no.nav.foreldrepenger.oppslag.lookup.Pair;
 import no.nav.foreldrepenger.oppslag.lookup.ws.aktor.AktorId;
 
 import java.util.Objects;
 
 public class ID {
 
-    private final Pair<AktorId, Fodselsnummer> values;
+    private final AktorId aktorId;
+    private final Fodselsnummer fnr;
 
     public ID(AktorId aktorId, Fodselsnummer fnr) {
-        values = Pair.of(aktorId, fnr);
+        this.aktorId = aktorId;
+        this.fnr = fnr;
     }
 
     public AktorId getAktorId() {
-        return values.getFirst();
+        return aktorId;
     }
 
     public Fodselsnummer getFnr() {
-        return values.getSecond();
+        return fnr;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ID id = (ID) o;
+        return Objects.equals(aktorId, id.aktorId) &&
+            Objects.equals(fnr, id.fnr);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(values);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        ID other = (ID) obj;
-        return Objects.equals(values, other.values);
+        return Objects.hash(aktorId, fnr);
     }
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " [values=" + values + ", aktorId=" + getAktorId() + ", fn)=" + getFnr()
-                + "]";
+        return "ID{" +
+            "aktorId=" + aktorId +
+            ", fnr=" + fnr +
+            '}';
     }
 }
