4e60881378998ecc1dd49247abb60880765cbab5
Jan-Olav Eide <janolaveide@gmail.com>
2018-05-30 08:44:54 +0200

    naughty boy checked in and broke the build, then went home. Won't happen again

--- src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/GradertUttaksPeriode.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/GradertUttaksPeriode.java
@@ -3,6 +3,9 @@ package no.nav.foreldrepenger.mottak.domain.foreldrepenger;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,7 +15,6 @@ import no.nav.foreldrepenger.mottak.domain.felles.Vedlegg;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-
 public class GradertUttaksPeriode extends UttaksPeriode {
     private double arbeidstidProsent;
     private boolean erArbeidstaker;
@@ -20,8 +22,12 @@ public class GradertUttaksPeriode extends UttaksPeriode {
     private boolean arbeidsForholdSomskalGraderes;
 
     @Builder
-    public GradertUttaksPeriode(LocalDate fom, LocalDate tom, List<Vedlegg> vedlegg,
-            UttaksperiodeType uttaksperiodeType, boolean ønskerSamtidigUttak, MorsAktivitetstype morsAktivitetsType) {
+    @JsonCreator
+    public GradertUttaksPeriode(@JsonProperty("fom") LocalDate fom, @JsonProperty("tom") LocalDate tom,
+            @JsonProperty("vedlegg") List<Vedlegg> vedlegg,
+            @JsonProperty("UttaksperiodeType") UttaksperiodeType uttaksperiodeType,
+            @JsonProperty("ønskerSamtidigUttak") boolean ønskerSamtidigUttak,
+            @JsonProperty("morsAktivitetsType") MorsAktivitetstype morsAktivitetsType) {
         super(fom, tom, vedlegg, uttaksperiodeType, ønskerSamtidigUttak, morsAktivitetsType);
 
     }
--- src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/UttaksPeriode.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/UttaksPeriode.java
@@ -1,8 +1,17 @@
 package no.nav.foreldrepenger.mottak.domain.foreldrepenger;
 
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,13 +21,21 @@ import no.nav.foreldrepenger.mottak.domain.felles.Vedlegg;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
+@JsonTypeInfo(use = NAME, include = PROPERTY, property = "type")
+@JsonSubTypes({
+        @Type(value = GradertUttaksPeriode.class, name = "gradert")
+})
 public class UttaksPeriode extends LukketPeriodeMedVedlegg {
     private final UttaksperiodeType uttaksperiodeType;
     private final boolean ønskerSamtidigUttak;
     private final MorsAktivitetstype morsAktivitetsType;
 
-    public UttaksPeriode(LocalDate fom, LocalDate tom, List<Vedlegg> vedlegg,
-            UttaksperiodeType uttaksperiodeType, boolean ønskerSamtidigUttak, MorsAktivitetstype morsAktivitetsType) {
+    @JsonCreator
+    public UttaksPeriode(@JsonProperty("fom") LocalDate fom, @JsonProperty("tom") LocalDate tom,
+            @JsonProperty("vedlegg") List<Vedlegg> vedlegg,
+            @JsonProperty("UttaksperiodeType") UttaksperiodeType uttaksperiodeType,
+            @JsonProperty("ønskerSamtidigUttak") boolean ønskerSamtidigUttak,
+            @JsonProperty("morsAktivitetsType") MorsAktivitetstype morsAktivitetsType) {
         super(fom, tom, vedlegg != null ? vedlegg : Collections.emptyList());
         this.uttaksperiodeType = uttaksperiodeType;
         this.ønskerSamtidigUttak = ønskerSamtidigUttak;
