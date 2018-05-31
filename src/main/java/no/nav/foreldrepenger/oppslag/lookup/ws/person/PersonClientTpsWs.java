7511ea2984f8f14b3be1cf4f57cb7168c06c8ae7
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-05-31 23:07:20 +0200

    now logging every person lookup + logging aktørid instead of fnr when person is not found

--- src/main/java/no/nav/foreldrepenger/oppslag/lookup/ws/person/PersonClientTpsWs.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/lookup/ws/person/PersonClientTpsWs.java
@@ -14,7 +14,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
-import static no.nav.foreldrepenger.oppslag.lookup.ws.person.RequestUtils.request;
 import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.BANKKONTO;
 import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.KOMMUNIKASJON;
 import static no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov.FAMILIERELASJONER;
@@ -40,9 +39,10 @@ public class PersonClientTpsWs implements PersonClient {
     public Person hentPersonInfo(ID id) {
 
         try {
+            LOG.info("Doing person lookup");
             HentPersonRequest request = RequestUtils.request(id.getFnr(), KOMMUNIKASJON, BANKKONTO, FAMILIERELASJONER);
             no.nav.tjeneste.virksomhet.person.v3.informasjon.Person tpsPerson =
-                hentPerson(id.getFnr(), request).getPerson();
+                hentPerson(id, request).getPerson();
             return PersonMapper.map(id, tpsPerson);
         } catch (Exception ex) {
             ERROR_COUNTER.increment();
@@ -51,11 +51,11 @@ public class PersonClientTpsWs implements PersonClient {
 
     }
 
-    private HentPersonResponse hentPerson(Fodselsnummer fnr, HentPersonRequest request) {
+    private HentPersonResponse hentPerson(ID id, HentPersonRequest request) {
         try {
             return person.hentPerson(request);
         } catch (HentPersonPersonIkkeFunnet e) {
-            LOG.warn("Kunne ikke slå opp person {}", fnr.getFnr(), e);
+            LOG.warn("Fant ikke person {}", id.getAktorId().getAktør(), e);
             throw new NotFoundException(e);
         } catch (HentPersonSikkerhetsbegrensning e) {
             LOG.warn("Sikkerhetsbegrensning ved oppslag.", e);
