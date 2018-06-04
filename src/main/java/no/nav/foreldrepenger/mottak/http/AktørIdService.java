3b39514ef9014319b6387efd6eff3aeb368831fd
Jan-Olav Eide <janolaveide@gmail.com>
2018-06-04 14:00:27 +0200

    Wrong default url to oppslag

--- src/main/java/no/nav/foreldrepenger/mottak/http/AktørIdService.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/AktørIdService.java
@@ -14,7 +14,7 @@ public class AktørIdService implements AktørIDLookup {
     private final RestTemplate template;
     private final URI aktørURI;
 
-    public AktørIdService(@Value("${fpoppslag.uri:http://fpsoknad-oppslag/api/aktor}") URI aktørURI,
+    public AktørIdService(@Value("${fpoppslag.uri:http://fpsoknad-oppslag/api/oppslag/aktor}") URI aktørURI,
             RestTemplate template) {
         this.template = template;
         this.aktørURI = aktørURI;
