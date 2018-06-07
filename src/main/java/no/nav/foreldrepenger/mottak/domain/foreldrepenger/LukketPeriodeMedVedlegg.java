bf14eda0b4e6233deb89f6db5e1256c7bc48263f
Jan-Olav Eide <janolaveide@gmail.com>
2018-06-07 10:58:51 +0200

    robustify, create empty list if we get null for lists in constructors

--- src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/LukketPeriodeMedVedlegg.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/LukketPeriodeMedVedlegg.java
@@ -2,10 +2,14 @@ package no.nav.foreldrepenger.mottak.domain.foreldrepenger;
 
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY;
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+import static java.util.Collections.emptyList;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
@@ -29,4 +33,12 @@ public abstract class LukketPeriodeMedVedlegg {
     private final LocalDate fom;
     private final LocalDate tom;
     private final List<String> vedlegg;
+
+    @JsonCreator
+    public LukketPeriodeMedVedlegg(@JsonProperty("fom") LocalDate fom, @JsonProperty("tom") LocalDate tom,
+            @JsonProperty("vedlegg") List<String> vedlegg) {
+        this.fom = fom;
+        this.tom = tom;
+        this.vedlegg = Optional.ofNullable(vedlegg).orElse(emptyList());
+    }
 }
