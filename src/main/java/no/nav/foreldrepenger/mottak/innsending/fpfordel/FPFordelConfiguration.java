a171585d93e586152e36c7945f5a8ddb3797eda9
Jan-Olav Eide <janolaveide@gmail.com>
2018-06-12 13:51:04 +0200

    add a couple of converters

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/fpfordel/FPFordelConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/fpfordel/FPFordelConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -14,6 +16,7 @@ public class FPFordelConfiguration {
         return new RestTemplateBuilder()
                 .rootUri(cfg.getUri())
                 .interceptors(interceptors)
+                .messageConverters(new MappingJackson2HttpMessageConverter(), new FormHttpMessageConverter())
                 .build();
     }
 }
