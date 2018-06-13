c89c934808cf347f177e061fa495e0c8055a6226
Jan-Olav Eide <janolaveide@gmail.com>
2018-06-13 08:53:14 +0200

    try again

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/fpfordel/FPFordelConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/fpfordel/FPFordelConfiguration.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -16,37 +15,28 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class FPFordelConfiguration {
+    private static final class Converter extends FormHttpMessageConverter {
+        @Override
+        public void setSupportedMediaTypes(List<MediaType> supportedMediaTypes) {
+            LOG.info("Types before {}", supportedMediaTypes);
+            LOG.info("Setting supported mediatypes to multipart/mixed");
+            super.setSupportedMediaTypes(
+                    Collections.singletonList(MediaType.parseMediaType("multipart/mixed")));
+        }
+    }
+
     private static final Logger LOG = LoggerFactory.getLogger(FPFordelConfiguration.class);
 
     @Bean
     public RestTemplate restTemplate(FPFordelConfig cfg, ClientHttpRequestInterceptor... interceptors) {
 
-        return new RestTemplateBuilder()
+        RestTemplate template = new RestTemplateBuilder()
                 .rootUri(cfg.getUri())
                 .interceptors(interceptors)
                 .build();
+        LOG.info("Adding converter for multipart/mixed");
+        template.getMessageConverters().add(new Converter());
+        return template;
     }
 
-    @Bean
-    public RestTemplateCustomizer customizer() {
-        LOG.info("Constructing template with customizer");
-        return new RestTemplateCustomizer() {
-
-            @Override
-            public void customize(RestTemplate restTemplate) {
-                restTemplate.getMessageConverters().add(new FormHttpMessageConverter() {
-
-                    @Override
-                    public void setSupportedMediaTypes(List<MediaType> supportedMediaTypes) {
-                        LOG.info("Types before {}", supportedMediaTypes);
-                        LOG.info("Setting supported mediatypes to multipart/mixed");
-                        super.setSupportedMediaTypes(
-                                Collections.singletonList(MediaType.parseMediaType("multipart/mixed")));
-                    }
-
-                });
-
-            }
-        };
-    }
 }
