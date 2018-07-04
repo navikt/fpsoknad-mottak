c766b5124a5628ff89e87328b69c3d71cf7a5d96
Jan-Olav Eide <janolaveide@gmail.com>
2018-07-04 09:09:43 +0200

    norgesopphold 2 perioder

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/fpfordel/FPFordelSøknadGenerator.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/fpfordel/FPFordelSøknadGenerator.java
@@ -13,11 +13,11 @@ import java.util.stream.Stream;
 
 import javax.xml.bind.JAXBContext;
 
-import org.assertj.core.util.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.Lists;
 import com.neovisionaries.i18n.CountryCode;
 
 import no.nav.foreldrepenger.mottak.domain.AktorId;
