bedd2525abda0d010124a638154c55015df23c1e
Jan-Olav Eide <janolaveide@gmail.com>
2018-08-02 11:32:46 +0200

    accept fornavn as navn

--- src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/NorskForelder.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/NorskForelder.java
@@ -1,6 +1,8 @@
 package no.nav.foreldrepenger.mottak.domain.foreldrepenger;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,10 +15,17 @@ import no.nav.foreldrepenger.mottak.domain.Fødselsnummer;
 public class NorskForelder extends AnnenForelder {
 
     private final Fødselsnummer fnr;
+    @JsonAlias("fornavn")
+    private final String navn;
+
+    public NorskForelder(@JsonProperty("fnr") Fødselsnummer fnr) {
+        this(fnr, null);
+    }
 
     @JsonCreator
-    public NorskForelder(Fødselsnummer fnr) {
+    public NorskForelder(@JsonProperty("fnr") Fødselsnummer fnr, @JsonProperty("navn") String navn) {
         this.fnr = fnr;
+        this.navn = navn;
     }
 
 }
