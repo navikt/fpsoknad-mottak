3101e7eb0b3287c1994f73dcf59ca56ab3ffaa82
Jan-Olav Eide <janolaveide@gmail.com>
2018-08-27 16:15:10 +0200

    added rest template bean

--- src/main/java/no/nav/foreldrepenger/OppslagApplication.java
+++ src/main/java/no/nav/foreldrepenger/OppslagApplication.java
@@ -2,14 +2,30 @@ package no.nav.foreldrepenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
 
+import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoConfig;
+import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoResponseErrorHandler;
 import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
 
 @SpringBootApplication
-@EnableOIDCTokenValidation(ignore="org.springframework")
+@EnableOIDCTokenValidation(ignore = "org.springframework")
 public class OppslagApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OppslagApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(FPInfoConfig cfg, ClientHttpRequestInterceptor... interceptors) {
+        RestTemplate template = new RestTemplateBuilder()
+                .rootUri(cfg.getFpinfo())
+                .interceptors(interceptors)
+                .errorHandler(new FPInfoResponseErrorHandler())
+                .build();
+        return template;
     }
 }
