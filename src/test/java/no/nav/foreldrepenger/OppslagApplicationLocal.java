7e2579d0e327a2d8578fad44d049422fa11ec72a
Jan-Olav Eide <janolaveide@gmail.com>
2018-08-27 16:09:29 +0200

    added rest template bean

--- src/test/java/no/nav/foreldrepenger/OppslagApplicationLocal.java
+++ src/test/java/no/nav/foreldrepenger/OppslagApplicationLocal.java
@@ -1,11 +1,17 @@
 package no.nav.foreldrepenger;
 
-import no.nav.security.spring.oidc.test.TokenGeneratorConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+
+import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoConfig;
+import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoResponseErrorHandler;
+import no.nav.security.spring.oidc.test.TokenGeneratorConfiguration;
+import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
 
 @SpringBootApplication
 @EnableOIDCTokenValidation(ignore = "org.springframework")
@@ -14,6 +20,16 @@ public class OppslagApplicationLocal {
 
     public static void main(String[] args) {
         SpringApplication.run(OppslagApplicationLocal.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(FPInfoConfig cfg, ClientHttpRequestInterceptor... interceptors) {
+        RestTemplate template = new RestTemplateBuilder()
+                .rootUri(cfg.getFpinfo())
+                .interceptors(interceptors)
+                .errorHandler(new FPInfoResponseErrorHandler())
+                .build();
+        return template;
     }
 
 }
