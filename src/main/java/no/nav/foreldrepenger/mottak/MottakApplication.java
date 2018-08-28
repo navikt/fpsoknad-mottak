f61d5310bbd030b7aa685edaa2154d883af01a59
Jan-Olav Eide <janolaveide@gmail.com>
2018-08-28 10:30:24 +0200

    added swagger annotation to main class

--- src/main/java/no/nav/foreldrepenger/mottak/MottakApplication.java
+++ src/main/java/no/nav/foreldrepenger/mottak/MottakApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableOIDCTokenValidation(ignore = { "org.springframework", "springfox.documentation" })
 @SpringBootApplication
+@EnableSwagger2
 public class MottakApplication {
 
     public static void main(String[] args) {
