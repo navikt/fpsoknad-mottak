145a853c0164379300ee485243b0b201ce143a48
Jan-Olav Eide <janolaveide@gmail.com>
2018-08-29 14:15:32 +0200

    we do not do behandling for now

--- src/main/java/no/nav/foreldrepenger/lookup/rest/fpinfo/FPInfoBehandlingsTema.java
+++
@@ -1,5 +0,0 @@
-package no.nav.foreldrepenger.lookup.rest.fpinfo;
-
-public enum FPInfoBehandlingsTema {
-    ENGST, ENGST_FODS, ENGST_ADOP, FORP, FORP_ADOP, FORP_FODS
-}
--- src/test/java/no/nav/foreldrepenger/TestFPInfoSerialization.java
+++ src/test/java/no/nav/foreldrepenger/TestFPInfoSerialization.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,11 +19,6 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
-import no.nav.foreldrepenger.lookup.rest.fpinfo.BehandlingsStatus;
-import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoBehandling;
-import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoBehandlingsTema;
-import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoBehandlingsType;
-import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoBehandlingsÅrsakType;
 import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoFagsakStatus;
 import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoSakStatus;
 
@@ -49,21 +43,6 @@ public class TestFPInfoSerialization {
     public void testFPInfoKvittering() throws Exception {
         FPInfoSakStatus status = new FPInfoSakStatus("42", FPInfoFagsakStatus.LOP, "Tema", "1", "2", null);
         test(status, true, mapper);
-    }
-
-    @Test
-    public void testBehandlingsStatus() throws Exception {
-        FPInfoBehandling behandling = new FPInfoBehandling(BehandlingsStatus.AVSLU, FPInfoBehandlingsType.BT002,
-                FPInfoBehandlingsTema.ENGST,
-                FPInfoBehandlingsÅrsakType.RE_OPPLYSNINGER_OM_BEREGNINGSGRUNNLAG, "402", "NAV",
-                Collections.emptyList());
-        test(behandling, true, mapper);
-    }
-
-    @Test
-    public void testBehandlingsType() throws Exception {
-        test(FPInfoBehandlingsÅrsakType.BERØRT_BEHANDLING, true, mapper);
-        test(FPInfoBehandlingsType.BT002, true, mapper);
     }
 
     private void test(Object object, boolean print) {
