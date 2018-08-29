0536fcb3007f6287220604b1de65c557557d3afb
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-08-29 08:59:40 +0200

    fixed duplicate rest template config

--- src/main/java/no/nav/foreldrepenger/OppslagApplication.java
+++ src/main/java/no/nav/foreldrepenger/OppslagApplication.java
@@ -1,15 +1,8 @@
 package no.nav.foreldrepenger;
 
+import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.web.client.RestTemplate;
-
-import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoConfig;
-import no.nav.foreldrepenger.lookup.rest.fpinfo.FPInfoResponseErrorHandler;
-import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
 
 @SpringBootApplication
 @EnableOIDCTokenValidation(ignore = { "org.springframework", "springfox.documentation" })
@@ -19,13 +12,4 @@ public class OppslagApplication {
         SpringApplication.run(OppslagApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate(FPInfoConfig cfg, ClientHttpRequestInterceptor... interceptors) {
-        RestTemplate template = new RestTemplateBuilder()
-                .rootUri(cfg.getFpinfo())
-                .interceptors(interceptors)
-                .errorHandler(new FPInfoResponseErrorHandler())
-                .build();
-        return template;
-    }
 }
--- src/main/java/no/nav/foreldrepenger/lookup/rest/fpinfo/FPInfoConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/lookup/rest/fpinfo/FPInfoConfiguration.java
@@ -1,12 +1,12 @@
 package no.nav.foreldrepenger.lookup.rest.fpinfo;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 
-@Configurable
+@Configuration
 public class FPInfoConfiguration {
     @Bean
     public RestTemplate restTemplate(FPInfoConfig cfg, ClientHttpRequestInterceptor... interceptors) {
