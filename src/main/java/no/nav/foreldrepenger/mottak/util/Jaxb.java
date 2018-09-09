438c99fcc7b64e5f7973d193a87cc3b386250192
Jan-Olav Eide <janolaveide@gmail.com>
2018-09-09 14:36:26 +0200

    fix test, fix equals for NorskForelder

--- src/main/java/no/nav/foreldrepenger/mottak/util/Jaxb.java
+++ src/main/java/no/nav/foreldrepenger/mottak/util/Jaxb.java
@@ -4,7 +4,6 @@ import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI;
 import static javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT;
 import static javax.xml.bind.Marshaller.JAXB_FRAGMENT;
 
-import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -14,10 +13,8 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.SchemaFactory;
 
-import org.springframework.core.io.ClassPathResource;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -83,17 +80,11 @@ public final class Jaxb {
 
     public static <T> JAXBElement<T> unmarshalToElement(String xml, JAXBContext context, Class<T> clazz) {
         try {
-            StreamSource[] sources = new StreamSource[5];
-            sources[0] = new StreamSource(new ClassPathResource("kodeverk/kodeverk.xsd").getInputStream());
-            sources[1] = new StreamSource(new ClassPathResource("felles/felles.xsd").getInputStream());
-            sources[2] = new StreamSource(new ClassPathResource("foreldrepenger/foreldrepenger.xsd").getInputStream());
-            sources[3] = new StreamSource(new ClassPathResource("uttak/uttak.xsd").getInputStream());
-            sources[4] = new StreamSource(new ClassPathResource("soeknad.xsd").getInputStream());
 
             Unmarshaller unmarshaller = unmarshaller(context);
             // unmarshaller.setSchema((SF.newSchema(sources)));
             return (JAXBElement<T>) unmarshaller.unmarshal(new StringReader(xml));
-        } catch (JAXBException | IOException e) {
+        } catch (JAXBException e) {
             e.printStackTrace();
             throw new IllegalArgumentException(e);
         }
