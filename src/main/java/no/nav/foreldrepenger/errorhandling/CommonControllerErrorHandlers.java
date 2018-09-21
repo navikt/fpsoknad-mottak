a2798204346f2086a518be694f5d26b46db4be2f
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-09-21 13:26:56 +0200

    logging all controller errors with level warn

--- src/main/java/no/nav/foreldrepenger/errorhandling/CommonControllerErrorHandlers.java
+++ src/main/java/no/nav/foreldrepenger/errorhandling/CommonControllerErrorHandlers.java
@@ -6,6 +6,9 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import javax.validation.ConstraintViolationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,42 +20,50 @@ import no.nav.security.spring.oidc.validation.interceptor.OIDCUnauthorizedExcept
 
 @ControllerAdvice
 public class CommonControllerErrorHandlers {
+
+    private static final Logger log = LoggerFactory.getLogger(CommonControllerErrorHandlers.class);
+
     private Counter invalidRequestsCounter = Metrics.counter("errors.request.invalid");
     private Counter forbiddenRequestsCounter = Metrics.counter("errors.lookup.forbidden");
 
     @ExceptionHandler({ ConstraintViolationException.class })
-    public ResponseEntity<String> handleValidationException(ConstraintViolationException e) {
+    public ResponseEntity<String> handleValidationException(ConstraintViolationException ex) {
         invalidRequestsCounter.increment();
-        return new ResponseEntity<>(e.getMessage(), BAD_REQUEST);
+        return logAndRespond(ex, BAD_REQUEST);
     }
 
     @ExceptionHandler({ IncompleteRequestException.class })
-    public ResponseEntity<String> handleIncompleteRequestException(IncompleteRequestException e) {
+    public ResponseEntity<String> handleIncompleteRequestException(IncompleteRequestException ex) {
         invalidRequestsCounter.increment();
-        return new ResponseEntity<>(e.getMessage(), BAD_REQUEST);
+        return logAndRespond(ex, BAD_REQUEST);
     }
 
     @ExceptionHandler({ NotFoundException.class })
-    public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), NOT_FOUND);
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return logAndRespond(ex, NOT_FOUND);
     }
 
     @ExceptionHandler({ ForbiddenException.class })
-    public ResponseEntity<String> handleForbiddenException(ForbiddenException e) {
+    public ResponseEntity<String> handleForbiddenException(ForbiddenException ex) {
         forbiddenRequestsCounter.increment();
-        return new ResponseEntity<>(e.getMessage(), FORBIDDEN);
+        return logAndRespond(ex, FORBIDDEN);
     }
 
     @ExceptionHandler({ MissingServletRequestParameterException.class })
     public ResponseEntity<String> handleMissingServletRequestParameterException(
-            MissingServletRequestParameterException e) {
+            MissingServletRequestParameterException ex) {
         invalidRequestsCounter.increment();
-        return new ResponseEntity<>(e.getMessage(), BAD_REQUEST);
+        return logAndRespond(ex, BAD_REQUEST);
     }
 
     @ExceptionHandler({ OIDCUnauthorizedException.class })
-    public ResponseEntity<String> handleOIDCUnauthorizedException(OIDCUnauthorizedException e) {
+    public ResponseEntity<String> handleOIDCUnauthorizedException(OIDCUnauthorizedException ex) {
         forbiddenRequestsCounter.increment();
-        return new ResponseEntity<>(e.getMessage(), FORBIDDEN);
+        return logAndRespond(ex, FORBIDDEN);
+    }
+
+    private ResponseEntity logAndRespond(Exception ex, HttpStatus status) {
+        log.warn("Caught error", ex);
+        return new ResponseEntity<>(ex.getMessage(), status);
     }
 }
