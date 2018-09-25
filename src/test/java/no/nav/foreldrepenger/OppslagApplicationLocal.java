bf51aabb348abc7214caf6adab9b589fafc9dc24
Jan-Olav Eide <janolaveide@gmail.com>
2018-09-25 12:13:49 +0200

    make  htis thing start on its own

--- src/test/java/no/nav/foreldrepenger/OppslagApplicationLocal.java
+++ src/test/java/no/nav/foreldrepenger/OppslagApplicationLocal.java
@@ -4,15 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import no.nav.security.oidc.test.support.spring.TokenGeneratorController;
+import no.nav.security.oidc.test.support.spring.TokenGeneratorConfiguration;
 
 @SpringBootApplication
-@no.nav.security.spring.oidc.api.EnableOIDCTokenValidation(ignore = "org.springframework")
-@Import(value = TokenGeneratorController.class)
+@no.nav.security.spring.oidc.api.EnableOIDCTokenValidation(ignore = { "org.springframework",
+        "springfox.documentation" })
+@Import(value = TokenGeneratorConfiguration.class)
 public class OppslagApplicationLocal {
 
     public static void main(String[] args) {
-        SpringApplication.run(OppslagApplicationLocal.class, args);
+        SpringApplication app = new SpringApplication(OppslagApplicationLocal.class);
+        app.setAdditionalProfiles("dev");
+        app.run(args);
     }
 
 }
