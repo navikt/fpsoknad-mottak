1920c955d39d8affef1955cb70b057f3d4a20d84
Jan-Olav Eide <janolaveide@gmail.com>
2018-09-26 14:00:33 +0200

    cleanup

--- src/main/java/no/nav/foreldrepenger/lookup/EnvUtil.java
+++ src/main/java/no/nav/foreldrepenger/lookup/EnvUtil.java
@@ -4,12 +4,15 @@ import org.springframework.core.env.Environment;
 
 public final class EnvUtil {
 
+    public static final String PREPROD = "preprod";
+    public static final String DEV = "dev";
+
     private EnvUtil() {
 
     }
 
     public static boolean isDevOrPreprod(Environment env) {
-        return env == null || env.acceptsProfiles("dev", "preprod");
+        return env == null || env.acceptsProfiles(DEV, PREPROD);
     }
 
     public static boolean isProd(Environment env) {
