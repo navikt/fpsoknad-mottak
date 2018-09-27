abb134bfcda6da608063923f1fb7086b237afeee
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-09-27 13:19:54 +0200

    corrected erred test

--- src/test/java/no/nav/foreldrepenger/lookup/rest/sak/StsClientTest.java
+++ src/test/java/no/nav/foreldrepenger/lookup/rest/sak/StsClientTest.java
@@ -15,7 +15,7 @@ public class StsClientTest {
     public void placeholdersInTemplateAreReplaced() {
         final StsClient stsClient = new StsClient(null, "http://whatever", "myuser", "mypw");
         final String processed = stsClient.replacePlaceholders("MY.OIDC.TOKEN");
-        assertTrue(processed.startsWith("<soap"));
+        assertTrue(processed.startsWith("<?xml"));
         assertTrue(processed.contains("<wsse:Username>myuser</wsse:Username>"));
         assertTrue(processed.contains("<wsse:Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText\">mypw</wsse:Password>"));
         assertTrue(processed.contains("MY.OIDC.TOKEN"));
