1ddb64a39131a8ece0c8b3e3da9fb6c026105da2
Jan-Olav Eide <janolaveide@gmail.com>
2018-10-01 15:34:08 +0200

    skip endringsøknad test for now

--- src/main/java/no/nav/foreldrepenger/mottak/http/SøknadController.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/SøknadController.java
@@ -2,7 +2,6 @@ package no.nav.foreldrepenger.mottak.http;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,15 +25,12 @@ import no.nav.foreldrepenger.mottak.domain.Sak;
 import no.nav.foreldrepenger.mottak.domain.Søker;
 import no.nav.foreldrepenger.mottak.domain.Søknad;
 import no.nav.foreldrepenger.mottak.domain.SøknadSender;
-import no.nav.foreldrepenger.mottak.domain.felles.DokumentType;
-import no.nav.foreldrepenger.mottak.domain.felles.ValgfrittVedlegg;
 import no.nav.foreldrepenger.mottak.domain.foreldrepenger.Endringssøknad;
 import no.nav.foreldrepenger.mottak.domain.foreldrepenger.Ettersending;
 import no.nav.foreldrepenger.mottak.domain.foreldrepenger.Fordeling;
 import no.nav.foreldrepenger.mottak.domain.foreldrepenger.LukketPeriodeMedVedlegg;
 import no.nav.foreldrepenger.mottak.domain.foreldrepenger.Overføringsårsak;
 import no.nav.foreldrepenger.mottak.innsyn.Innsyn;
-import no.nav.foreldrepenger.mottak.util.EnvUtil;
 import no.nav.security.oidc.api.ProtectedWithClaims;
 import no.nav.security.oidc.api.Unprotected;
 
@@ -86,25 +81,17 @@ public class SøknadController {
     @GetMapping(value = "/saker")
     public List<Sak> saker() {
         List<Sak> saker = innsyn.hentSaker(oppslag.getAktørId());
-        if (EnvUtil.isDevOrPreprod(env)) {
-            try {
-                if (!saker.isEmpty()) {
-                    String saksnummer = saker.get(0).getSaksnummer();
-                    LOG.trace(EnvUtil.CONFIDENTIAL, "Tester endringssøknad mot sak {}", saksnummer);
-                    ValgfrittVedlegg vedlegg = new ValgfrittVedlegg(DokumentType.I500005,
-                            new ClassPathResource("sykkel.pdf"));
-                    Endringssøknad es = new Endringssøknad(søker(), fordeling(), null, null, null,
-                            saksnummer,
-                            vedlegg);
-                    sender.send(es, oppslag.getSøker());
-                }
-                else {
-                    LOG.trace("Ingen saker å ettersende til");
-                }
-            } catch (IOException e) {
-                LOG.error("Funkade inte, men dette var bare en test", e);
-            }
-        }
+        /*
+         * if (EnvUtil.isDevOrPreprod(env)) { try { if (!saker.isEmpty()) { String
+         * saksnummer = saker.get(0).getSaksnummer(); LOG.trace(EnvUtil.CONFIDENTIAL,
+         * "Tester endringssøknad mot sak {}", saksnummer); ValgfrittVedlegg vedlegg =
+         * new ValgfrittVedlegg(DokumentType.I500005, new
+         * ClassPathResource("sykkel.pdf")); Endringssøknad es = new
+         * Endringssøknad(søker(), fordeling(), null, null, null, saksnummer, vedlegg);
+         * sender.send(es, oppslag.getSøker()); } else {
+         * LOG.trace("Ingen saker å ettersende til"); } } catch (IOException e) {
+         * LOG.error("Funkade inte, men dette var bare en test", e); } }
+         */
         return saker;
 
     }
