524d750c0d269836775b268b6060a7cf7a44d986
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-10-01 09:30:07 +0200

    removed unnecessary logging

--- src/main/java/no/nav/foreldrepenger/lookup/rest/sak/SakClientHttp.java
+++ src/main/java/no/nav/foreldrepenger/lookup/rest/sak/SakClientHttp.java
@@ -1,16 +1,13 @@
 package no.nav.foreldrepenger.lookup.rest.sak;
 
-import no.nav.foreldrepenger.lookup.EnvUtil;
 import no.nav.foreldrepenger.lookup.ws.aktor.AktorId;
 import no.nav.foreldrepenger.lookup.ws.ytelser.Sak;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.env.Environment;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.inject.Inject;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Base64;
@@ -28,9 +25,6 @@ public class SakClientHttp implements SakClient {
 
     private StsClient stsClient;
 
-    @Inject
-    private Environment env;
-
     public SakClientHttp(String sakBaseUrl, RestTemplate restTemplate, StsClient stsClient) {
         this.sakBaseUrl = sakBaseUrl;
         this.restTemplate = restTemplate;
@@ -43,9 +37,6 @@ public class SakClientHttp implements SakClient {
         String samlToken = stsClient.exchangeForSamlToken(oidcToken);
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Saml " + encode(samlToken));
-        if (EnvUtil.isDevOrPreprod(env)) {
-            log.debug(EnvUtil.CONFIDENTIAL, headers.get("Authorization").toString());
-        }
         headers.set("X-Correlation-ID", "bogus");
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
