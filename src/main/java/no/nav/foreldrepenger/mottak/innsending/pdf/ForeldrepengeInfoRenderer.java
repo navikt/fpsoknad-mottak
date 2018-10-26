c58a5bc7bcbb73fcf1f71adda9970935350cf3c5
Jan-Olav Eide <janolaveide@gmail.com>
2018-10-26 13:30:27 +0200

    gender neutral

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/ForeldrepengeInfoRenderer.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/ForeldrepengeInfoRenderer.java
@@ -274,7 +274,8 @@ public class ForeldrepengeInfoRenderer {
         FramtidigOppholdsInformasjon framtidigeOpphold = medlemsskap.getFramtidigOppholdsInfo();
         String land = framtidigeOpphold.isFødselNorge() ? "Norge" : "utlandet";
         if (relasjonTilBarn instanceof FremtidigFødsel) {
-            y -= renderer.addLineOfRegularText(INDENT, txt("terminføderi", land), cos, y);
+            y -= renderer.addLineOfRegularText(INDENT,
+                    txt("terminføderi", relasjonTilBarn.getAntallBarn() > 1 ? "a" : "et", land), cos, y);
         }
         if (relasjonTilBarn instanceof Fødsel) {
             y -= renderer.addLineOfRegularText(INDENT, txt("føderi", land), cos, y);
