f049b63912dc69c8f4bb314d266c9dc7d4b0b322
Jan-Olav Eide <janolaveide@gmail.com>
2018-10-28 22:54:34 +0100

    revert

--- src/main/java/no/nav/foreldrepenger/mottak/domain/serialization/LocalDateTimeAdapter.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/serialization/LocalDateTimeAdapter.java
@@ -1,7 +1,5 @@
 package no.nav.foreldrepenger.mottak.domain.serialization;
 
-import static java.time.format.DateTimeFormatter.ISO_DATE;
-
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -9,8 +7,8 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
     @Override
-    public LocalDateTime unmarshal(String dato) throws Exception {
-        return LocalDateTime.parse(dato, ISO_DATE);
+    public LocalDateTime unmarshal(String v) throws Exception {
+        return LocalDateTime.parse(v);
     }
 
     @Override
