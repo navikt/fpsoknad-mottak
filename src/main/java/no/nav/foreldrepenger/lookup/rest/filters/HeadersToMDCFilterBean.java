574d9c4ec4b5a49aa70a230806385cb0ec4f25c6
Jan-Olav Eide <janolaveide@gmail.com>
2018-11-06 14:17:56 +0100

    one filter will do

--- src/main/java/no/nav/foreldrepenger/lookup/rest/filters/HeadersToMDCFilterBean.java
+++ src/main/java/no/nav/foreldrepenger/lookup/rest/filters/HeadersToMDCFilterBean.java
@@ -1,8 +1,10 @@
 package no.nav.foreldrepenger.lookup.rest.filters;
 
+import static no.nav.foreldrepenger.lookup.Constants.NAV_AKTØR_ID;
 import static no.nav.foreldrepenger.lookup.Constants.NAV_CALL_ID;
 import static no.nav.foreldrepenger.lookup.Constants.NAV_CONSUMER_ID;
-import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+import static no.nav.foreldrepenger.lookup.Constants.NAV_USER_ID;
+import static no.nav.foreldrepenger.lookup.EnvUtil.isDevOrPreprod;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -14,25 +16,33 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import no.nav.foreldrepenger.lookup.CallIdGenerator;
+import no.nav.foreldrepenger.lookup.TokenHandler;
+import no.nav.foreldrepenger.lookup.ws.aktor.AktorIdClient;
 
 @Component
-@Order(HIGHEST_PRECEDENCE)
 public class HeadersToMDCFilterBean extends GenericFilterBean {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HeadersToMDCFilterBean.class);
 
     private final CallIdGenerator generator;
     private final String applicationName;
+    private final TokenHandler tokenHandler;
+    private final AktorIdClient aktørIdClient;
 
     @Inject
-    public HeadersToMDCFilterBean(CallIdGenerator generator,
+    public HeadersToMDCFilterBean(CallIdGenerator generator, TokenHandler tokenHandler, AktorIdClient aktørIdClient,
             @Value("${spring.application.name}") String applicationName) {
         this.generator = generator;
+        this.tokenHandler = tokenHandler;
+        this.aktørIdClient = aktørIdClient;
         this.applicationName = applicationName;
     }
 
@@ -46,6 +56,18 @@ public class HeadersToMDCFilterBean extends GenericFilterBean {
     private void putValues(HttpServletRequest request) {
         putValue(NAV_CONSUMER_ID, request.getHeader(NAV_CONSUMER_ID), applicationName);
         putValue(NAV_CALL_ID, request.getHeader(NAV_CALL_ID), generator.create());
+        try {
+            if (isDevOrPreprod(getEnvironment())) {
+                putValue(NAV_USER_ID, tokenHandler.getSubject().getFnr());
+            }
+            putValue(NAV_AKTØR_ID, aktørIdClient.aktorIdForFnr(tokenHandler.getSubject()).getAktør());
+        } catch (Exception e) {
+            LOG.warn("Noe gikk feil, MDC-verdier er inkomplette", e);
+        }
+    }
+
+    private static void putValue(String key, String value) {
+        putValue(key, value, null);
     }
 
     private static void putValue(String key, String value, String defaultValue) {
@@ -54,7 +76,8 @@ public class HeadersToMDCFilterBean extends GenericFilterBean {
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " [generator=" + generator + ", applicationName=" + applicationName + "]";
+        return getClass().getSimpleName() + " [generator=" + generator + ", applicationName=" + applicationName
+                + ", tokenHandler=" + tokenHandler + ", aktørIdClient=" + aktørIdClient + "]";
     }
 
 }
--- src/main/java/no/nav/foreldrepenger/lookup/rest/filters/HeadersToMDCFilterRegistrationBean.java
+++ src/main/java/no/nav/foreldrepenger/lookup/rest/filters/HeadersToMDCFilterRegistrationBean.java
@@ -5,16 +5,18 @@ import static no.nav.foreldrepenger.lookup.rest.filters.FilterRegistrationUtil.u
 import static no.nav.foreldrepenger.lookup.rest.sak.SakController.SAK;
 import static no.nav.foreldrepenger.lookup.ws.arbeidsforhold.ArbeidsforholdController.ARBEIDSFORHOLD;
 import static no.nav.foreldrepenger.lookup.ws.person.PersonController.PERSON;
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(HIGHEST_PRECEDENCE)
 public class HeadersToMDCFilterRegistrationBean extends FilterRegistrationBean<HeadersToMDCFilterBean> {
 
     public HeadersToMDCFilterRegistrationBean(HeadersToMDCFilterBean headersFilter) {
         setFilter(headersFilter);
         setUrlPatterns(urlPatternsFor(OPPSLAG, ARBEIDSFORHOLD, PERSON, SAK));
     }
-
 }
