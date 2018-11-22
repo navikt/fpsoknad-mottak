5b128fbbbdeda7f1a0cf6b2cf1236088a4d61a96
Jan-Olav Eide <janolaveide@gmail.com>
2018-11-22 14:41:09 +0100

    separate tests

--- src/test/java/no/nav/foreldrepenger/mottak/domain/validation/ProsentValidatorTest.java
+++ src/test/java/no/nav/foreldrepenger/mottak/domain/validation/ProsentValidatorTest.java
@@ -41,29 +41,54 @@ public class ProsentValidatorTest {
     }
 
     @Test
-    public void test100Prosent() {
-        assertStuff(true, 100d, 100d);
-        assertStuff(true, "50%", 50d);
-        assertStuff(true, 50d, 50d);
-        assertStuff(-50d);
-        assertStuff(true, 0d, 0d);
-        assertStuff(100.1d);
+    public void test100() {
+        testProsent(true, 100d);
     }
 
-    private void assertStuff(double value) {
-        assertStuff(false, value, value);
+    @Test
+    public void testMedProsentTegn() {
+        testProsent(true, "50%", 50d);
+    }
+
+    @Test
+    public void testOK() {
+        testProsent(50d);
+    }
+
+    @Test
+    public void testNegativ() {
+        testProsent(false, -50d);
+    }
+
+    @Test
+    public void test0() {
+        testProsent(0d);
+    }
+
+    @Test
+    public void testOver100() {
+        testProsent(false, 100.1d);
+    }
+
+    private void testProsent(boolean shouldBeEmpty, double value) {
+        testProsent(shouldBeEmpty, value, value);
 
     }
 
-    private void assertStuff(boolean shouldBeEmpty, String value, double expectedValue) {
-        assertStuff(shouldBeEmpty, new ProsentBruker(ProsentAndel.valueOf(value)), expectedValue);
+    private void testProsent(double value) {
+        testProsent(true, value, value);
+
     }
 
-    private void assertStuff(boolean shouldBeEmpty, double value, double expectedValue) {
-        assertStuff(shouldBeEmpty, new ProsentBruker(new ProsentAndel(value)), expectedValue);
+    private void testProsent(boolean shouldBeEmpty, String value, double expectedValue) {
+        testProsent(shouldBeEmpty, new ProsentBruker(ProsentAndel.valueOf(value)), expectedValue);
     }
 
-    private static void assertStuff(boolean shouldBeEmpty, ProsentBruker prosent, double expectedValue) {
+    private void testProsent(boolean shouldBeEmpty, double value, double expectedValue) {
+        testProsent(shouldBeEmpty, new ProsentBruker(new ProsentAndel(value)), expectedValue);
+    }
+
+    private static void testProsent(boolean shouldBeEmpty, ProsentBruker prosent, double expectedValue) {
         Set<ConstraintViolation<ProsentBruker>> constraintViolations = validator.validate(prosent);
         if (shouldBeEmpty) {
             assertTrue(constraintViolations.isEmpty());
