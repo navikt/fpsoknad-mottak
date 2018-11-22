99aa3f3205b9011ed36817d26d660f2766a3636b
Jan-Olav Eide <janolaveide@gmail.com>
2018-11-22 15:21:40 +0100

    test CommonsRequestLoggingFilter in preprod

--- src/main/java/no/nav/foreldrepenger/mottak/config/MottakConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/MottakConfiguration.java
@@ -1,10 +1,14 @@
 package no.nav.foreldrepenger.mottak.config;
 
+import static no.nav.foreldrepenger.mottak.util.EnvUtil.PREPROD;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class MottakConfiguration {
@@ -23,5 +27,17 @@ public class MottakConfiguration {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("kvitteringstekster");
         return messageSource;
+    }
+
+    @Bean
+    @Profile(PREPROD)
+    public CommonsRequestLoggingFilter loggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
     }
 }
