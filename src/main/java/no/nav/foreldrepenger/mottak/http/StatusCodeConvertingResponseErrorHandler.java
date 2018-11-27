64b2be6aeb7b5228bf3eae768288664aa79cc2f0
Jan-Olav Eide <janolaveide@gmail.com>
2018-11-27 13:52:10 +0100

    added error handler to be used soon

---
+++ src/main/java/no/nav/foreldrepenger/mottak/http/StatusCodeConvertingResponseErrorHandler.java
@@ -0,0 +1,47 @@
+package no.nav.foreldrepenger.mottak.http;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.HttpClientErrorException;
+
+import no.nav.foreldrepenger.mottak.http.errorhandling.NotFoundException;
+import no.nav.foreldrepenger.mottak.http.errorhandling.UnauthenticatedException;
+import no.nav.foreldrepenger.mottak.http.errorhandling.UnauthorizedException;
+import no.nav.foreldrepenger.mottak.util.TokenHandler;
+
+//TODO, not yet used
+public class StatusCodeConvertingResponseErrorHandler extends DefaultResponseErrorHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StatusCodeConvertingResponseErrorHandler.class);
+    private final TokenHandler tokenHandler;
+
+    public StatusCodeConvertingResponseErrorHandler(TokenHandler tokenHandler) {
+        this.tokenHandler = tokenHandler;
+    }
+
+    @Override
+    protected void handleError(ClientHttpResponse res, HttpStatus code) throws IOException {
+        LOG.info("Handling error code {}", code);
+        switch (code) {
+        case NOT_FOUND:
+            throw new NotFoundException(res.getStatusText(), new HttpClientErrorException(code));
+        case UNAUTHORIZED:
+            throw new UnauthorizedException(res.getStatusText(), new HttpClientErrorException(code));
+        case FORBIDDEN:
+            throw new UnauthenticatedException(res.getStatusText(), tokenHandler.getExp(),
+                    new HttpClientErrorException(code));
+        default:
+            super.handleError(res, code);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [tokenHandler=" + tokenHandler + "]";
+    }
+}
