a9ff536cbd70fd4ce0acbda70454366b4eab7996
Jan-Olav Eide <janolaveide@gmail.com>
2018-11-27 09:57:40 +0100

    trigger filter always

--- src/main/java/no/nav/foreldrepenger/mottak/http/filters/HeadersToMDCFilterRegistrationBean.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/filters/HeadersToMDCFilterRegistrationBean.java
@@ -1,8 +1,6 @@
 package no.nav.foreldrepenger.mottak.http.filters;
 
-import static no.nav.foreldrepenger.mottak.http.controllers.InnsynController.INNSYN;
-import static no.nav.foreldrepenger.mottak.http.controllers.SøknadController.INNSENDING;
-import static no.nav.foreldrepenger.mottak.http.filters.FilterRegistrationUtil.urlPatternsFor;
+import java.util.Collections;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.stereotype.Component;
@@ -12,6 +10,7 @@ public class HeadersToMDCFilterRegistrationBean extends FilterRegistrationBean<H
 
     public HeadersToMDCFilterRegistrationBean(HeadersToMDCFilterBean headersFilter) {
         setFilter(headersFilter);
-        setUrlPatterns(urlPatternsFor(INNSENDING, INNSYN));
+        setUrlPatterns(Collections.singletonList("/*"));
+        // setUrlPatterns(urlPatternsFor(INNSENDING, INNSYN));
     }
 }
