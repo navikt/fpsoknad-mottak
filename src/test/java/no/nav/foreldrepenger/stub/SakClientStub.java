73e13dfa34cda6029f2ff8594e80d2a2d6e0345a
Jan-Olav Eide <janolaveide@gmail.com>
2018-12-07 11:33:08 +0100

    token har nothing to in a method signature at service layer

--- src/test/java/no/nav/foreldrepenger/stub/SakClientStub.java
+++ src/test/java/no/nav/foreldrepenger/stub/SakClientStub.java
@@ -1,25 +1,24 @@
 package no.nav.foreldrepenger.stub;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 import io.micrometer.core.annotation.Timed;
 import no.nav.foreldrepenger.lookup.rest.sak.Sak;
 import no.nav.foreldrepenger.lookup.rest.sak.SakClient;
 import no.nav.foreldrepenger.lookup.ws.aktor.AktorId;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-
 public class SakClientStub implements SakClient {
     @Override
     @Timed("lookup.sak")
-    public List<Sak> sakerFor(AktorId aktor, String oidcToken) {
+    public List<Sak> sakerFor(AktorId aktor) {
         return Arrays.asList(
-            new Sak("sak1", "typen", "systemet", "fsid1",
-                "status", LocalDate.of(2018,9,19), ""),
-            new Sak("sak2", "typen", "systemet", "fsid2",
-                "status", LocalDate.of(2018,9,18), ""),
-            new Sak("sak3", "typen", "systemet", "fsid3",
-                "status", LocalDate.of(2018,9,17), "")
-        );
+                new Sak("sak1", "typen", "systemet", "fsid1",
+                        "status", LocalDate.of(2018, 9, 19), ""),
+                new Sak("sak2", "typen", "systemet", "fsid2",
+                        "status", LocalDate.of(2018, 9, 18), ""),
+                new Sak("sak3", "typen", "systemet", "fsid3",
+                        "status", LocalDate.of(2018, 9, 17), ""));
     }
 }
