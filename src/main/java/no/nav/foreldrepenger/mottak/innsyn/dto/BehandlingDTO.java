331375d979ea1b9995d1709df580f94afedc5edb
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-04 11:18:20 +0100

    delombokify BehandlingDTO

--- src/main/java/no/nav/foreldrepenger/mottak/innsyn/dto/BehandlingDTO.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsyn/dto/BehandlingDTO.java
@@ -10,11 +10,10 @@ import java.util.Optional;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
 import no.nav.foreldrepenger.mottak.innsyn.Lenke;
 
-@Data
 public class BehandlingDTO {
+
     private static final String SØKNAD = "søknad";
     private final String status;
     private final String type;
@@ -52,6 +51,54 @@ public class BehandlingDTO {
         this.behandlingResultatType = behandlingResultatType;
         this.inntekstmeldinger = Optional.ofNullable(inntekstmeldinger).orElse(emptyList());
         this.lenker = Optional.ofNullable(lenker).orElse(emptyList());
+    }
+
+    public static String getSøknad() {
+        return SØKNAD;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public String getÅrsak() {
+        return årsak;
+    }
+
+    public String getBehandlendeEnhet() {
+        return behandlendeEnhet;
+    }
+
+    public String getBehandlendeEnhetNavn() {
+        return behandlendeEnhetNavn;
+    }
+
+    public String getBehandlingResultatType() {
+        return behandlingResultatType;
+    }
+
+    public LocalDateTime getOpprettetTidspunkt() {
+        return opprettetTidspunkt;
+    }
+
+    public LocalDateTime getEndretTidspunkt() {
+        return endretTidspunkt;
+    }
+
+    public List<String> getInntekstmeldinger() {
+        return inntekstmeldinger;
+    }
+
+    public List<Lenke> getLenker() {
+        return lenker;
     }
 
     public Lenke getSøknadsLenke() {
