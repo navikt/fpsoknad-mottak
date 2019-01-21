dcd9479a258d29223b7ab4435c89c81bfef5ec05
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-21 16:21:37 +0100

    log status value

--- src/main/java/no/nav/foreldrepenger/mottak/errorhandling/MottakExceptionHandler.java
+++ src/main/java/no/nav/foreldrepenger/mottak/errorhandling/MottakExceptionHandler.java
@@ -103,7 +103,7 @@ public class MottakExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, HttpHeaders headers,
             List<Object> messages) {
         ApiError apiError = apiErrorFra(status, e, req, messages);
-        LOG.warn("{} [{}] ", status, apiError.getMessages(), e);
+        LOG.warn("{} {} ({})", status, apiError.getMessages(), status.value(), e);
         return handleExceptionInternal(e, apiError, headers, status, req);
     }
 
