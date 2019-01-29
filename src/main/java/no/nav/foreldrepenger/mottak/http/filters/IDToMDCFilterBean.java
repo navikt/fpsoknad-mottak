6d504e2a5c6aee86c3036407e3728b948aad1fb7
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-29 12:17:14 +0100

    do exp date last

--- src/main/java/no/nav/foreldrepenger/mottak/http/filters/IDToMDCFilterBean.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/filters/IDToMDCFilterBean.java
@@ -1,8 +1,10 @@
 package no.nav.foreldrepenger.mottak.http.filters;
 
 import static no.nav.foreldrepenger.mottak.Constants.NAV_AKTØR_ID;
+import static no.nav.foreldrepenger.mottak.Constants.NAV_TOKEN_EXPIRY_ID;
 import static no.nav.foreldrepenger.mottak.Constants.NAV_USER_ID;
 import static no.nav.foreldrepenger.mottak.util.EnvUtil.isDevOrPreprod;
+import static no.nav.foreldrepenger.mottak.util.MDCUtil.toMDC;
 import static org.springframework.core.Ordered.LOWEST_PRECEDENCE;
 
 import java.io.IOException;
@@ -29,17 +31,17 @@ public class IDToMDCFilterBean extends GenericFilterBean {
     private static final Logger LOG = LoggerFactory.getLogger(IDToMDCFilterBean.class);
 
     private final Oppslag oppslag;
-    private final TokenUtil tokenHelper;
+    private final TokenUtil tokenUtil;
 
-    public IDToMDCFilterBean(TokenUtil tokenHelper, Oppslag oppslag) {
-        this.tokenHelper = tokenHelper;
+    public IDToMDCFilterBean(TokenUtil tokenUtil, Oppslag oppslag) {
+        this.tokenUtil = tokenUtil;
         this.oppslag = oppslag;
     }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
-        if (tokenHelper.erAutentisert()) {
+        if (tokenUtil.erAutentisert()) {
             copyHeadersToMDC();
         }
         chain.doFilter(req, res);
@@ -48,16 +50,18 @@ public class IDToMDCFilterBean extends GenericFilterBean {
     private void copyHeadersToMDC() {
         try {
             if (isDevOrPreprod(getEnvironment())) {
-                MDC.put(NAV_USER_ID, tokenHelper.autentisertBruker().getFnr());
+                MDC.put(NAV_USER_ID, tokenUtil.autentisertBruker().getFnr());
             }
+            toMDC(NAV_TOKEN_EXPIRY_ID, tokenUtil.getExpiryDate());
             MDC.put(NAV_AKTØR_ID, oppslag.getAktørId().getId());
+
         } catch (Exception e) {
-            LOG.trace("Noe gikk feil ved oppslag av aktørid, MDC-verdier er inkomplette", e);
+            LOG.warn("Noe gikk feil ved oppslag av aktørid, MDC-verdier er inkomplette", e);
         }
     }
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " [oppslag=" + oppslag + ", tokenHelper=" + tokenHelper + "]";
+        return getClass().getSimpleName() + " [oppslag=" + oppslag + ", tokenHelper=" + tokenUtil + "]";
     }
 }
