e94233007f24549fb1bdda0da53e77ef8d659f81
Christian H. Mosveen <mosveen@gmail.com>
2019-03-05 10:35:46 +0100

    Return PDF also in stub mode

--- src/main/java/no/nav/foreldrepenger/mottak/domain/serialization/NorskForelderSerializer.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/serialization/NorskForelderSerializer.java
@@ -22,8 +22,7 @@ public class NorskForelderSerializer extends StdSerializer<NorskForelder> {
     }
 
     @Override
-    public void serialize(NorskForelder forelder, JsonGenerator gen, SerializerProvider provider)
-            throws IOException {
+    public void serialize(NorskForelder forelder, JsonGenerator gen, SerializerProvider provider) throws IOException {
         gen.writeStringField("fnr", forelder.getFnr().getFnr());
         gen.writeStringField("navn", forelder.getNavn());
     }
