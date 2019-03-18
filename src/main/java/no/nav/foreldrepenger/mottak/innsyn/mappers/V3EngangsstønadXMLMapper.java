0440d7c40d3746c9e4582e0772eb72c12fc70971
Jan-Olav Eide <janolaveide@gmail.com>
2019-03-18 13:36:44 +0100

    added missing mapper  for V3 ES

---
+++ src/main/java/no/nav/foreldrepenger/mottak/innsyn/mappers/V3EngangsstønadXMLMapper.java
@@ -0,0 +1,38 @@
+package no.nav.foreldrepenger.mottak.innsyn.mappers;
+
+import static no.nav.foreldrepenger.mottak.innsending.SøknadType.INITIELL_ENGANGSSTØNAD;
+import static no.nav.foreldrepenger.mottak.util.Versjon.V3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import no.nav.foreldrepenger.mottak.domain.Søknad;
+import no.nav.foreldrepenger.mottak.innsending.mappers.MapperEgenskaper;
+import no.nav.foreldrepenger.mottak.innsyn.SøknadEgenskap;
+import no.nav.foreldrepenger.mottak.oppslag.Oppslag;
+
+@Component
+public class V3EngangsstønadXMLMapper extends AbstractXMLMapper {
+
+    private static final MapperEgenskaper EGENSKAPER = new MapperEgenskaper(V3, INITIELL_ENGANGSSTØNAD);
+
+    private static final Logger LOG = LoggerFactory.getLogger(V3EngangsstønadXMLMapper.class);
+
+    public V3EngangsstønadXMLMapper(Oppslag oppslag) {
+        super(oppslag);
+    }
+
+    @Override
+    public MapperEgenskaper mapperEgenskaper() {
+        return EGENSKAPER;
+    }
+
+    @Override
+    public Søknad tilSøknad(String xml, SøknadEgenskap egenskap) {
+        // TODO
+        LOG.info("Mapper ikke engangssøknad V3 tilbake til søknad foreløpig");
+        return null;
+    }
+
+}
