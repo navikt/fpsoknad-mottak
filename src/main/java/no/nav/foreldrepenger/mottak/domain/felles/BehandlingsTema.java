7e7b527f549f244dde9d4d0e8e4a094d2d3a5e53
Christian H. Mosveen <mosveen@gmail.com>
2019-03-28 15:23:37 +0100

    Add behandlingstema ENGST

--- src/main/java/no/nav/foreldrepenger/mottak/domain/felles/BehandlingsTema.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/felles/BehandlingsTema.java
@@ -1,5 +1,5 @@
 package no.nav.foreldrepenger.mottak.domain.felles;
 
 public enum BehandlingsTema {
-    FORP_FODS, FORP_ADOP, ENGST_FODS, ENGST_ADOP
+    FORP_FODS, FORP_ADOP, ENGST_FODS, ENGST_ADOP, FORP, ENGST
 }
