7d67a46636567ba2c22fd95f8addcdcff1496c21
Jan-Olav Eide <janolaveide@gmail.com>
2019-04-16 08:58:42 +0200

    better tests

--- src/test/java/no/nav/foreldrepenger/mottak/domain/validation/ProsentValidatorTest.java
+++ src/test/java/no/nav/foreldrepenger/mottak/domain/validation/ProsentValidatorTest.java
@@ -2,7 +2,6 @@ package no.nav.foreldrepenger.mottak.domain.validation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import javax.validation.Valid;
 import javax.validation.Validation;
 import javax.validation.Validator;
 
@@ -12,19 +11,14 @@ import no.nav.foreldrepenger.mottak.domain.validation.annotations.Prosent;
 
 public class ProsentValidatorTest {
 
-    public class ProsentBruker {
+    private static class ProsentBruker {
 
-        @Valid
         @Prosent
         private final Double prosent;
         @Prosent(max = 200, min = -10)
         private final Double prosent1;
 
-        public ProsentBruker(Double prosent) {
-            this(prosent, prosent);
-        }
-
-        public ProsentBruker(Double prosent, Double prosent1) {
+        ProsentBruker(Double prosent, Double prosent1) {
             this.prosent = prosent;
             this.prosent1 = prosent1;
         }
@@ -34,52 +28,52 @@ public class ProsentValidatorTest {
 
     @Test
     public void testMax() {
-        testProsent(true, 100);
+        test(true, 100);
     }
 
     @Test
     public void testMax2() {
-        testProsent(true, 100, 200);
+        test(true, 100, 200);
     }
 
     @Test
     public void testOverMax() {
-        testProsent(false, 100, 201);
+        test(false, 100, 201);
     }
 
     @Test
     public void testUnderMin() {
-        testProsent(false, -50);
+        test(false, -50);
     }
 
     @Test
     public void testUnderMin2() {
-        testProsent(false, -2, -20);
+        test(false, -2, -20);
     }
 
     @Test
     public void testMin() {
-        testProsent(0);
+        test(0);
     }
 
     @Test
     public void testMin2() {
-        testProsent(true, 0, -10);
+        test(true, 0, -10);
     }
 
-    private void testProsent(Number value) {
-        testProsent(true, value);
+    private static void test(Number value) {
+        test(true, value);
     }
 
-    private void testProsent(boolean shouldBeEmpty, Number value) {
-        testProsent(shouldBeEmpty, value, value);
+    private static void test(boolean empty, Number value) {
+        test(empty, value, value);
     }
 
-    private void testProsent(boolean shouldBeEmpty, Number value, Number value1) {
-        testProsent(shouldBeEmpty, new ProsentBruker(value.doubleValue(), value1.doubleValue()));
+    private static void test(boolean empty, Number value, Number value1) {
+        test(empty, new ProsentBruker(value.doubleValue(), value1.doubleValue()));
     }
 
-    private static void testProsent(boolean shouldBeEmpty, ProsentBruker prosent) {
-        assertEquals(shouldBeEmpty, VALIDATOR.validate(prosent).isEmpty());
+    private static void test(boolean empty, ProsentBruker prosent) {
+        assertEquals(empty, VALIDATOR.validate(prosent).isEmpty());
     }
 }
