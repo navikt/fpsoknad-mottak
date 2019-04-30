8a1464d03e29ebe68c538487485d97e83a260153
Jan-Olav Eide <janolaveide@gmail.com>
2019-04-30 12:23:02 +0200

    revert

--- src/main/java/no/nav/foreldrepenger/mottak/innsyn/uttaksplan/UtsettelsePeriodeType.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsyn/uttaksplan/UtsettelsePeriodeType.java
@@ -1,11 +1,8 @@
 package no.nav.foreldrepenger.mottak.innsyn.uttaksplan;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public enum UtsettelsePeriodeType {
     ARBEID,
     LOVBESTEMT_FERIE,
-    @JsonProperty("LOVBESTEMT_FERIE")
     FERIE,
     SYKDOM_SKADE,
     SØKER_INNLAGT,
