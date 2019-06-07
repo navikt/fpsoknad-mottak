c93443df427a098cf3238c7fde2be07194adbb0e
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-07 15:31:13 +0200

    disable producer  for now

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/LoggingDomainEventPublisher.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/LoggingDomainEventPublisher.java
@@ -2,14 +2,13 @@ package no.nav.foreldrepenger.mottak.innsending.foreldrepenger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import no.nav.foreldrepenger.mottak.domain.Kvittering;
 import no.nav.foreldrepenger.mottak.innsyn.SøknadEgenskap;
 
 @Component
-@ConditionalOnProperty(value = "mottak.sender.domainevent.enabled", havingValue = "false", matchIfMissing = true)
+//@ConditionalOnProperty(value = "mottak.sender.domainevent.enabled", havingValue = "false", matchIfMissing = true)
 public class LoggingDomainEventPublisher implements InnsendingDomainEventPublisher {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoggingDomainEventPublisher.class);
