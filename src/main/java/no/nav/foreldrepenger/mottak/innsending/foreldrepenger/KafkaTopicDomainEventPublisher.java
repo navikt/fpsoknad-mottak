ba688478915db407f7ea3a01dc686d75486d652e
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-07 21:48:30 +0200

    try again

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/KafkaTopicDomainEventPublisher.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/KafkaTopicDomainEventPublisher.java
@@ -20,8 +20,6 @@ public class KafkaTopicDomainEventPublisher implements InnsendingDomainEventPubl
     private static final Logger LOG = LoggerFactory.getLogger(KafkaTopicDomainEventPublisher.class);
     private final String topic;
     private final KafkaOperations<String, Kvittering> KafkaOperations;
-    @Value("${kafka.password}")
-    private String pw;
 
     public KafkaTopicDomainEventPublisher(@Value("${mottak.sender.domainevent.topic}") String topic,
             KafkaTemplate<String, Kvittering> KafkaOperations) {
@@ -31,7 +29,7 @@ public class KafkaTopicDomainEventPublisher implements InnsendingDomainEventPubl
 
     @Override
     public void publishEvent(Kvittering kvittering, SøknadEgenskap egenskap, List<String> vedlegg) {
-        LOG.info("{}  Publiserer hendelse fra {} for søknad {} med vedlegg {}", pw, kvittering, egenskap, vedlegg);
+        LOG.info("Publiserer hendelse fra {} for søknad {} med vedlegg {}", kvittering, egenskap, vedlegg);
         KafkaOperations.send(topic, kvittering);
     }
 
