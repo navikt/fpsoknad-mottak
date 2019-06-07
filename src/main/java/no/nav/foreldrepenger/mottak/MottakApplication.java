264522dfa0b6f06305be4fc57c2ac7d881c2dd8e
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-07 22:07:13 +0200

    @EnableKafka

--- src/main/java/no/nav/foreldrepenger/mottak/MottakApplication.java
+++ src/main/java/no/nav/foreldrepenger/mottak/MottakApplication.java
@@ -3,12 +3,14 @@ package no.nav.foreldrepenger.mottak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.kafka.annotation.EnableKafka;
 
 import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
 
 @EnableOIDCTokenValidation(ignore = { "org.springframework", "springfox.documentation" })
 @SpringBootApplication
 @EnableCaching
+@EnableKafka
 public class MottakApplication {
     public static void main(String[] args) {
         SpringApplication.run(MottakApplication.class, args);
