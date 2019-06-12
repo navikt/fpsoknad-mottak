5d0ed7aa826a39e7b197179d947cf1c7d4f07130
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-12 11:38:35 +0200

    log

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/SøknadMottattMeldingKonsument.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/SøknadMottattMeldingKonsument.java
@@ -1,11 +1,13 @@
 package no.nav.foreldrepenger.mottak.innsending.foreldrepenger;
 
+import static no.nav.foreldrepenger.mottak.Constants.NAV_CALL_ID;
 import static no.nav.foreldrepenger.mottak.util.EnvUtil.PREPROD;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +16,7 @@ public class SøknadMottattMeldingKonsument {
     private static final Logger LOG = LoggerFactory.getLogger(SøknadMottattMeldingKonsument.class);
 
     @KafkaListener(topics = "#{'${mottak.sender.domainevent.topic}'}", groupId = "#{'${spring.kafka.consumer.group-id}'}")
-    public void listen(String json) {
-        LOG.info("Mottok melding");
+    public void listen(String json, @Header(NAV_CALL_ID) String callId) {
+        LOG.info("Mottok søknad {}", callId);
     }
 }
