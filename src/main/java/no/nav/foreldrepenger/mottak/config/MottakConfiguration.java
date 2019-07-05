1a64871c77685b40ca7557beef64278565d1a575
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-07-05 12:56:24 +0200

    vault

--- src/main/java/no/nav/foreldrepenger/mottak/config/MottakConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/MottakConfiguration.java
@@ -1,6 +1,9 @@
 package no.nav.foreldrepenger.mottak.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +11,17 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 public class MottakConfiguration {
+    @Value("${testpreprod:jalla}")
+    private String test;
+
+    private static final Logger LOG = LoggerFactory.getLogger(MottakConfiguration.class);
     public static final String LANDKODER = "landkoder";
     public static final String KVITTERINGSTEKSTER = "kvitteringstekster";
 
     @Bean
     @Qualifier(LANDKODER)
     public MessageSource landkoder() {
+        LOG.info("XXXXXXXXXXXXXX " + test);
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename(LANDKODER);
         return messageSource;
