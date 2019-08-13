147797a8155bff74b13b7f431267cf09efba8e26
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-08-13 16:02:49 +0200

    added config for prod

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,2 @@
 FROM navikt/java:11
-
-ARG version
-ARG app_name
-
-ENV LC_ALL="no_NB.UTF-8"
-ENV LANG="no_NB.UTF-8"
-ENV TZ="Europe/Oslo"
 COPY target/*.jar app.jar
--- src/main/java/no/nav/foreldrepenger/mottak/config/MottakConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/MottakConfiguration.java
@@ -1,9 +1,6 @@
 package no.nav.foreldrepenger.mottak.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +8,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 public class MottakConfiguration {
-    @Value("${test:jalla}")
-    private String test;
 
-    private static final Logger LOG = LoggerFactory.getLogger(MottakConfiguration.class);
     public static final String LANDKODER = "landkoder";
     public static final String KVITTERINGSTEKSTER = "kvitteringstekster";
 
