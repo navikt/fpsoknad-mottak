67764f4a022aa05c1442b8ce175d174eb94549a2
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2019-08-14 09:53:36 +0200

    PFP-7598 kjønnsnøytral tekst ved opphold pga annen forelders kvoteuttak

--- src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/fordeling/Oppholdsårsak.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/foreldrepenger/fordeling/Oppholdsårsak.java
@@ -1,9 +1,12 @@
 package no.nav.foreldrepenger.mottak.domain.foreldrepenger.fordeling;
 
 public enum Oppholdsårsak {
-    INGEN, UTTAK_MØDREKVOTE_ANNEN_FORELDER, UTTAK_FEDREKVOTE_ANNEN_FORELDER, UTTAK_FORELDREPENGER_ANNEN_FORELDER,
-    UTTAK_FELLESP_ANNEN_FORELDER(
-            "periode.opphold.uttakfellesannen");
+    INGEN,
+    UTTAK_MØDREKVOTE_ANNEN_FORELDER("periode.opphold.uttakkvoteannen"),
+    UTTAK_FEDREKVOTE_ANNEN_FORELDER("periode.opphold.uttakkvoteannen"),
+    UTTAK_FORELDREPENGER_ANNEN_FORELDER,
+    UTTAK_FELLESP_ANNEN_FORELDER("periode.opphold.uttakfellesannen");
+
     private final String key;
 
     public String getKey() {
