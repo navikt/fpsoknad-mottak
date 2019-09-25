9ee8ecd22d3cf86f174ce84211780e4d714f620f
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-09-25 15:49:40 +0200

    cleanup

--- src/main/java/no/nav/foreldrepenger/mottak/config/ClusterAwareSpringProfileResolver.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/ClusterAwareSpringProfileResolver.java
@@ -1,15 +1,20 @@
 package no.nav.foreldrepenger.mottak.config;
 
+import static java.lang.System.getenv;
 import static no.nav.foreldrepenger.mottak.util.EnvUtil.DEFAULT;
 import static no.nav.foreldrepenger.mottak.util.EnvUtil.DEV;
 import static no.nav.foreldrepenger.mottak.util.EnvUtil.LOCAL;
+
+import java.util.Optional;
 
 public class ClusterAwareSpringProfileResolver {
 
     private static final String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
 
-    public String getProfile() {
-        return clusterFra(System.getenv(NAIS_CLUSTER_NAME));
+    public static String[] profiles() {
+        return Optional.ofNullable(clusterFra(getenv(NAIS_CLUSTER_NAME)))
+                .map(c -> new String[] { c })
+                .orElse(new String[0]);
     }
 
     private static String clusterFra(String cluster) {
