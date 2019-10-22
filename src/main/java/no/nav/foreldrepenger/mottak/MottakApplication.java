74eb94728d57d330d3be8c052ed05c4516d97f4d
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-10-22 12:19:52 +0200

    enable retry

--- src/main/java/no/nav/foreldrepenger/mottak/MottakApplication.java
+++ src/main/java/no/nav/foreldrepenger/mottak/MottakApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.retry.annotation.EnableRetry;
 
 import no.nav.foreldrepenger.mottak.config.ClusterAwareSpringProfileResolver;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
@@ -11,6 +12,7 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 @EnableJwtTokenValidation(ignore = { "org.springframework", "springfox.documentation" })
 @SpringBootApplication
 @EnableCaching
+@EnableRetry
 @EnableKafka
 public class MottakApplication {
     public static void main(String[] args) {
