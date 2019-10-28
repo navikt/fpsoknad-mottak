737810ff804cb53bd4ef1856cc0ad6be01eb67de
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-10-28 16:26:00 +0100

    we log subject always, no need to handle 401 and 403 explicitly

--- src/main/java/no/nav/foreldrepenger/mottak/error/MottakExceptionHandler.java
+++ src/main/java/no/nav/foreldrepenger/mottak/error/MottakExceptionHandler.java
@@ -10,8 +10,6 @@ import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 import java.util.List;
 import java.util.Optional;
 
-import javax.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -22,7 +20,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -33,17 +30,12 @@ import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnaut
 
 @ControllerAdvice
 public class MottakExceptionHandler extends ResponseEntityExceptionHandler {
-    @Inject
-    private TokenUtil tokenUtil;
+
+    private final TokenUtil tokenUtil;
     private static final Logger LOG = LoggerFactory.getLogger(MottakExceptionHandler.class);
 
-    @ResponseBody
-    @ExceptionHandler(HttpStatusCodeException.class)
-    public ResponseEntity<Object> handleHttpStatusCodeException(HttpStatusCodeException e, WebRequest request) {
-        if (e.getStatusCode().equals(UNAUTHORIZED) || e.getStatusCode().equals(FORBIDDEN)) {
-            return logAndHandle(e.getStatusCode(), e, request, tokenUtil.getExpiryDate());
-        }
-        return logAndHandle(e.getStatusCode(), e, request);
+    public MottakExceptionHandler(TokenUtil tokenUtil) {
+        this.tokenUtil = tokenUtil;
     }
 
     @Override
@@ -58,27 +50,32 @@ public class MottakExceptionHandler extends ResponseEntityExceptionHandler {
         return logAndHandle(UNPROCESSABLE_ENTITY, e, req, headers);
     }
 
-    @ExceptionHandler({ JwtTokenUnauthorizedException.class })
+    @ExceptionHandler(HttpStatusCodeException.class)
+    public ResponseEntity<Object> handleHttpStatusCodeException(HttpStatusCodeException e, WebRequest request) {
+        return logAndHandle(e.getStatusCode(), e, request);
+    }
+
+    @ExceptionHandler(JwtTokenUnauthorizedException.class)
     public ResponseEntity<Object> handleUnauthorizedException(JwtTokenUnauthorizedException e, WebRequest req) {
         return logAndHandle(UNAUTHORIZED, e, req);
     }
 
-    @ExceptionHandler({ SøknadEgenskapException.class })
+    @ExceptionHandler(SøknadEgenskapException.class)
     public ResponseEntity<Object> handleVersionException(SøknadEgenskapException e, WebRequest req) {
         return logAndHandle(UNPROCESSABLE_ENTITY, e, req, e.getVersjon());
     }
 
-    @ExceptionHandler({ UnexpectedInputException.class })
+    @ExceptionHandler(UnexpectedInputException.class)
     public ResponseEntity<Object> handleIncompleteException(UnexpectedInputException e, WebRequest req) {
         return logAndHandle(UNPROCESSABLE_ENTITY, e, req);
     }
 
-    @ExceptionHandler({ JwtTokenValidatorException.class })
+    @ExceptionHandler(JwtTokenValidatorException.class)
     public ResponseEntity<Object> handleUnauthenticatedJwtException(JwtTokenValidatorException e, WebRequest req) {
-        return logAndHandle(FORBIDDEN, e, req, e.getExpiryDate());
+        return logAndHandle(FORBIDDEN, e, req);
     }
 
-    @ExceptionHandler(value = { Exception.class })
+    @ExceptionHandler(Exception.class)
     protected ResponseEntity<Object> handleUncaught(Exception e, WebRequest req) {
         return logAndHandle(INTERNAL_SERVER_ERROR, e, req);
     }
