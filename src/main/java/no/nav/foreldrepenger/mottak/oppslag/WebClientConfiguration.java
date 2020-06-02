13afa8191cf1b4a9a736cd0d887b9192ead358d2
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-02 14:57:22 +0200

    reactive test

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/WebClientConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/WebClientConfiguration.java
@@ -1,9 +1,14 @@
 package no.nav.foreldrepenger.mottak.oppslag;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfiguration {
@@ -15,10 +20,19 @@ public class WebClientConfiguration {
     private String servicePwd;
 
     @Bean
-    public WebClient webClient(WebClient.Builder builder, @Value("${sts.uri}") String url) {
-        return builder.baseUrl(url)
-                .defaultHeaders(header -> header.setBasicAuth(serviceUser, servicePwd))
-                .build();
+    public WebClient webClient(ExchangeFilterFunction... filters) {
+        var builder = WebClient.builder().defaultHeaders(header -> header.setBasicAuth(serviceUser, servicePwd));
+        Arrays.stream(filters).forEach(builder::filter);
+        return builder.build();
     }
 
+    @Bean
+    public ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            clientRequest
+                    .headers()
+                    .forEach((name, values) -> System.out.println(name + "->" + values));
+            return Mono.just(clientRequest);
+        });
+    }
 }
