461f11760f5542ccdbb114a852e488ec5386c95c
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-02 18:28:20 +0200

    test

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/SystemUserTokenService.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/SystemUserTokenService.java
@@ -2,26 +2,27 @@ package no.nav.foreldrepenger.mottak.oppslag;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
-//@Service
+@Service
 public class SystemUserTokenService {
 
     private static final Logger LOG = LoggerFactory.getLogger(SystemUserTokenService.class);
 
-    // private final WebClient webClient;
+    private final WebClient webClient;
 
-    /*
-     * public SystemUserTokenService(WebClient webClient) { this.webClient =
-     * webClient; // fetch(); }
-     */
+    public SystemUserTokenService(WebClient webClient) {
+        this.webClient = webClient;
+        fetch();
+    }
 
-    /*
-     * public UserToken fetch() { LOG.trace("Henter JWT token for service user");
-     * var token = webClient.get().accept(MediaType.APPLICATION_JSON).retrieve()
-     * .bodyToMono(UserToken.class).block();
-     * LOG.trace("Hentet JWT token for service user {}", token); return token;
-     * 
-     * }
-     */
-
+    public UserToken fetch() {
+        LOG.trace("Henter JWT token for service user");
+        var token = webClient.get().accept(MediaType.APPLICATION_JSON).retrieve()
+                .bodyToMono(UserToken.class).block();
+        LOG.trace("Hentet JWT token for service user {}", token);
+        return token;
+    }
 }
