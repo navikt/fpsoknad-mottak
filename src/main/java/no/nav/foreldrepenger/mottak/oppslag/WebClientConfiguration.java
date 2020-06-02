b31bfea47e60424af971cd13ae725c7ad3a22ba5
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-02 18:38:13 +0200

    test

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/WebClientConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/WebClientConfiguration.java
@@ -8,33 +8,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
 
     private static final Logger LOG = LoggerFactory.getLogger(WebClientConfiguration.class);
-    private final String serviceUser;
-
-    private final String servicePwd;
-
-    public WebClientConfiguration(@Value("${kafka.username}") String serviceUser,
-            @Value("${kafka.password}") String servicePwd) {
-        this.serviceUser = serviceUser;
-        this.servicePwd = servicePwd;
-    }
 
     @Bean
-    public WebClient webClient(ExchangeFilterFunction... filters) {
-        var builder = WebClient.builder().defaultHeaders(header -> header.setBasicAuth(serviceUser, servicePwd));
+    public WebClient webClient(@Value("${sts.uri}") String uri, @Value("${kafka.username}") String serviceUser,
+            @Value("${kafka.password}") String servicePwd, ExchangeFilterFunction... filters) {
+        var builder = WebClient.builder().baseUrl(uri)
+                .defaultHeaders(header -> header.setBasicAuth(serviceUser, servicePwd));
         Arrays.stream(filters).forEach(builder::filter);
         return builder.build();
     }
-
-    @Bean
-    public ExchangeFilterFunction authenticate() {
-        return ExchangeFilterFunctions.basicAuthentication(serviceUser, servicePwd);
-    }
-
 }
