e1a86a044d34812a82f31af0e219a6b46bedf740
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-02 17:30:58 +0200

    reactive

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/WebClientConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/WebClientConfiguration.java
@@ -6,11 +6,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebClientConfiguration {
 
-    @Value("${kafka.username}")
-    private String serviceUser;
+    private final String serviceUser;
 
-    @Value("${kafka.password}")
-    private String servicePwd;
+    private final String servicePwd;
     /*
      * 
      * @Bean public WebClient webClient(ExchangeFilterFunction... filters) { var
@@ -23,4 +21,10 @@ public class WebClientConfiguration {
      * .headers() .forEach((name, values) -> System.out.println(name + "->" +
      * values)); return Mono.just(clientRequest); }); }
      */
+
+    public WebClientConfiguration(@Value("${kafka.username}") String serviceUser,
+            @Value("${kafka.password}") String servicePwd) {
+        this.serviceUser = serviceUser;
+        this.servicePwd = servicePwd;
+    }
 }
