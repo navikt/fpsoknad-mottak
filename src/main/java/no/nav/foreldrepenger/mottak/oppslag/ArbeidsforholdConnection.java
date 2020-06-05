e3a2ef3f0c14a79973128c78cb6123a436cfcb13
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-05 11:25:35 +0200

    get json for now

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/ArbeidsforholdConnection.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/ArbeidsforholdConnection.java
@@ -11,7 +11,6 @@ import java.net.URI;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -69,7 +68,7 @@ public class ArbeidsforholdConnection implements PingEndpointAware {
                         .build())
                 .accept(APPLICATION_JSON)
                 .retrieve()
-                .toEntityList(Map.class) // TODO
+                .toEntityList(String.class) // TODO
                 .block()
                 .getBody();
         LOG.trace("Hentet arbeidsforhold {}", forhold);
