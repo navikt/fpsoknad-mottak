5653f2c9d1672d07b590d112f4dfd96ea9d74667
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-15 22:14:26 +0200

    ingen retry på not found

--- src/main/java/no/nav/foreldrepenger/mottak/http/RetryAware.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/RetryAware.java
@@ -2,13 +2,20 @@ package no.nav.foreldrepenger.mottak.http;
 
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpClientErrorException.Forbidden;
 import org.springframework.web.client.HttpServerErrorException.BadGateway;
 import org.springframework.web.client.HttpServerErrorException.InternalServerError;
 import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-@Retryable(include = { ResourceAccessException.class,
-        BadGateway.class }, exclude = { InternalServerError.class,
+@Retryable(include = {
+        ResourceAccessException.class,
+        WebClientResponseException.class,
+        BadGateway.class }, exclude = {
+                HttpClientErrorException.NotFound.class,
+                WebClientResponseException.NotFound.class,
+                InternalServerError.class,
                 Forbidden.class }, maxAttemptsExpression = "#{${rest.retry.attempts:3}}", backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
 
 public interface RetryAware {
