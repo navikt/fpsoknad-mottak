a398d31077b5f08dcdc21846960bcf5597311880
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-16 13:31:21 +0200

    fix isAlive path

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/FordelConfig.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/FordelConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FordelConfig {
 
-    public static final String PING_PATH = "fpfordel/internal/isReady";
+    public static final String PING_PATH = "fpfordel/internal/health/isAlive";
     public static final String BASE_PATH = "fpfordel/api/dokumentforsendelse";
 
     public static final URI DEFAULT_URI = URI.create("http://fpfordel");
