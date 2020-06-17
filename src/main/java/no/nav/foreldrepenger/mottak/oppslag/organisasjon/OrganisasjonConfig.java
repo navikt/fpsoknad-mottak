6c95784e31bfa0048ab4a2d382ed45db25a83db4
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-17 07:38:13 +0200

    health

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/organisasjon/OrganisasjonConfig.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/organisasjon/OrganisasjonConfig.java
@@ -1,5 +1,7 @@
 package no.nav.foreldrepenger.mottak.oppslag.organisasjon;
 
+import static org.springframework.web.util.UriComponentsBuilder.newInstance;
+
 import java.net.URI;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,14 +14,14 @@ import no.nav.foreldrepenger.mottak.oppslag.AbstractConfig;
 @ConfigurationProperties(prefix = "organisasjon")
 public class OrganisasjonConfig extends AbstractConfig {
 
-    private static final String PING = "ping";
     private static final String V1_ORGANISASJON = "/v1/organisasjon/";
+    private static final String NAV = "998004993";
     private final String organisasjonPath;
 
     @ConstructorBinding
-    public OrganisasjonConfig(String baseUri, @DefaultValue(PING) String pingPath,
+    public OrganisasjonConfig(String baseUri,
             @DefaultValue(V1_ORGANISASJON) String organisasjonPath, boolean log) {
-        super(baseUri, pingPath, log);
+        super(baseUri, newInstance().path(organisasjonPath).pathSegment(NAV).build().toUriString(), log);
         this.organisasjonPath = organisasjonPath;
     }
 
@@ -34,4 +36,5 @@ public class OrganisasjonConfig extends AbstractConfig {
         return getClass().getSimpleName() + "[organisasjonPath=" + organisasjonPath + ", pingEndpoint="
                 + pingEndpoint() + ", log=" + isLog() + "]";
     }
+
 }
