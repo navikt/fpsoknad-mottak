a0817da633efe31b16a0db8cbb64539a6529f5fa
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-06-22 23:19:31 +0200

    org navn

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/organisasjon/OrganisasjonMapper.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/organisasjon/OrganisasjonMapper.java
@@ -29,7 +29,8 @@ public class OrganisasjonMapper {
             String navn5 = get(navn, NAVNELINJE5);
             String normalisertNavn = get(navn, REDIGERTNAVN);
             LOG.info("Fikk navn 1={} 2={} 3={} 4={} 5={} ({})", navn1, navn2, navn3, navn4, navn5, normalisertNavn);
-            return navn1;
+            return navn3 != null ? navn1 + ", " + navn3 : navn1;
+            // return navn1;
         } catch (Exception e) {
             LOG.warn("OOPS", e);
             return "UKJENT";
