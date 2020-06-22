5881fcb9332a29d83d04c71b15422bec9b7a7450
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-06-22 19:17:48 +0200

    log navn

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/organisasjon/OrganisasjonMapper.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/organisasjon/OrganisasjonMapper.java
@@ -4,13 +4,35 @@ import static no.nav.foreldrepenger.mottak.util.MapUtil.get;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class OrganisasjonMapper {
 
     private static final String REDIGERTNAVN = "redigertnavn";
+    private static final String NAVNELINJE1 = "navnelinje1";
+    private static final String NAVNELINJE2 = "navnelinje2";
+    private static final String NAVNELINJE3 = "navnelinje3";
+    private static final String NAVNELINJE4 = "navnelinje4";
+    private static final String NAVNELINJE5 = "navnelinje5";
+
+    private static final Logger LOG = LoggerFactory.getLogger(OrganisasjonMapper.class);
     private static final String NAVN = "navn";
 
     public static String map(Map<?, ?> map) {
+        try {
+            String navn1 = get(get(map, NAVN, Map.class), NAVNELINJE1);
+            String navn2 = get(get(map, NAVN, Map.class), NAVNELINJE2);
+            String navn3 = get(get(map, NAVN, Map.class), NAVNELINJE3);
+            String navn4 = get(get(map, NAVN, Map.class), NAVNELINJE4);
+            String navn5 = get(get(map, NAVN, Map.class), NAVNELINJE5);
+            LOG.info("Fikk navn {} {} {} {} {}", navn1, navn2, navn3, navn4, navn5);
+        } catch (Exception e) {
+            LOG.warn("OOPS ", e);
+        }
+
         return get(get(map, NAVN, Map.class), REDIGERTNAVN);
+
     }
 
 }
