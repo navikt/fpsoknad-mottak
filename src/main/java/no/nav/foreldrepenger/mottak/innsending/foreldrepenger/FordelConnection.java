2323b5c73f9dcef953644ed899a01a1418343388
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-06-22 11:39:07 +0200

    fjernet timing vi ikke bruker

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/FordelConnection.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/foreldrepenger/FordelConnection.java
@@ -1,25 +1,18 @@
 package no.nav.foreldrepenger.mottak.innsending.foreldrepenger;
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static no.nav.foreldrepenger.mottak.domain.Kvittering.ikkeSendt;
 import static no.nav.foreldrepenger.mottak.util.CounterRegistry.FP_SENDFEIL;
 import static no.nav.foreldrepenger.mottak.util.URIUtil.uri;
 
 import java.net.URI;
-import java.time.Duration;
-import java.util.Optional;
 
-import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
 import no.nav.foreldrepenger.mottak.domain.BrukerRolle;
 import no.nav.foreldrepenger.mottak.domain.Kvittering;
-import no.nav.foreldrepenger.mottak.domain.LeveranseStatus;
 import no.nav.foreldrepenger.mottak.http.AbstractRestConnection;
 import no.nav.foreldrepenger.mottak.innsending.PingEndpointAware;
 import no.nav.foreldrepenger.mottak.innsending.SøknadType;
@@ -31,14 +24,12 @@ public class FordelConnection extends AbstractRestConnection implements PingEndp
 
     private final FordelConfig config;
     private final ResponseHandler responseHandler;
-    private final MeterRegistry registry;
 
     public FordelConnection(RestOperations restOperations, FordelConfig config,
-            ResponseHandler responseHandler, MeterRegistry registry) {
+            ResponseHandler responseHandler) {
         super(restOperations);
         this.config = config;
         this.responseHandler = responseHandler;
-        this.registry = registry;
     }
 
     public Kvittering send(Konvolutt konvolutt, BrukerRolle rolle) {
@@ -51,40 +42,19 @@ public class FordelConnection extends AbstractRestConnection implements PingEndp
 
     private Kvittering doSend(Konvolutt konvolutt, BrukerRolle rolle) {
         try {
-            StopWatch stopWatch = new StopWatch();
-            stopWatch.start();
             LOG.info("Sender {} til {}", name(konvolutt.getType()), name().toLowerCase());
             var kvittering = responseHandler
                     .handle(postForEntity(uri(config.getUri(), config.getBasePath()), konvolutt.getPayload(),
                             FordelKvittering.class));
-            stopWatch.stop();
             kvittering.setPdf(konvolutt.PDFHovedDokument());
             LOG.info("Sendte {} til {}, fikk kvittering {}", name(konvolutt.getType()), name().toLowerCase(),
                     kvittering);
             konvolutt.getType().count();
-            timer(konvolutt.getType(), rolle, kvittering.getLeveranseStatus()).record(stopWatch.getTime(),
-                    MILLISECONDS);
             return kvittering;
         } catch (Exception e) {
             FP_SENDFEIL.increment();
             throw e;
         }
-    }
-
-    private Timer timer(SøknadType type, BrukerRolle rolle, LeveranseStatus leveranseStatus) {
-        return Timer.builder("application.send")
-                .tags("rolle", rolleFra(rolle), "status", leveranseStatus.name(), "type", type.name(), "fagsaktype",
-                        type.fagsakType().name())
-                .publishPercentiles(0.5, 0.95)
-                .publishPercentileHistogram()
-                .minimumExpectedValue(Duration.ofSeconds(2))
-                .maximumExpectedValue(Duration.ofSeconds(20)).register(registry);
-    }
-
-    private static String rolleFra(BrukerRolle rolle) {
-        return Optional.ofNullable(rolle)
-                .map(BrukerRolle::name)
-                .orElse("N/A");
     }
 
     @Override
