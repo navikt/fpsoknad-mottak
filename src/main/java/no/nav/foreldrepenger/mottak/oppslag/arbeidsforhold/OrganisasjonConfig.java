2772ac460a1b061181050a0c3d67960f44634871
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-06-24 12:05:50 +0200

    uriVAriables

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/OrganisasjonConfig.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/OrganisasjonConfig.java
@@ -14,7 +14,7 @@ import no.nav.foreldrepenger.mottak.oppslag.AbstractConfig;
 @ConfigurationProperties(prefix = "organisasjon")
 public class OrganisasjonConfig extends AbstractConfig {
 
-    private static final String V1_ORGANISASJON = "/v1/organisasjon/";
+    private static final String V1_ORGANISASJON = "/v1/organisasjon/{orgnr}";
     private static final String NAV = "998004993";
     private final String organisasjonPath;
 
@@ -27,8 +27,7 @@ public class OrganisasjonConfig extends AbstractConfig {
 
     URI getOrganisasjonURI(UriBuilder b, String orgnr) {
         return b.path(organisasjonPath)
-                .pathSegment(orgnr)
-                .build();
+                .build(orgnr);
     }
 
     @Override
