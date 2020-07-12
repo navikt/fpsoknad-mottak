dac7471cd54092f198c70477d6bf40ed58f7e676
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-07-12 22:19:13 +0200

    cleanup pdf  config

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/pdftjeneste/PdfGeneratorConfig.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/pdftjeneste/PdfGeneratorConfig.java
@@ -1,64 +1,55 @@
 package no.nav.foreldrepenger.mottak.innsending.pdf.pdftjeneste;
 
+import static no.nav.foreldrepenger.mottak.util.URIUtil.uri;
+
 import java.net.URI;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
-import no.nav.foreldrepenger.mottak.util.URIUtil;
-
 @ConfigurationProperties(prefix = "fppdfgen")
 public class PdfGeneratorConfig {
     private static final String DEFAULT_URI = "http://fppdfgen";
     private static final String DEFAULT_BASE_PATH = "/api/v1/genpdf/";
-    private static final String DEFAULT_PING_PATH = DEFAULT_BASE_PATH + "is_alive";
-    static final String SØKNAD = DEFAULT_BASE_PATH + "soknad-v2/soknad";
+    private static final String DEFAULT_PING_PATH = "is_alive";
+    private static final String DEFAULT_SØKNAD_PATH = "soknad-v2/soknad";
 
-    private final URI uri;
-    private final String pingPath;
-    private final String basePath;
     private final boolean enabled;
+    private final URI pdfUri;
+    private final URI pingUri;
 
     @ConstructorBinding
-    public PdfGeneratorConfig(@DefaultValue(DEFAULT_URI) URI uri,
+    public PdfGeneratorConfig(@DefaultValue(DEFAULT_URI) URI baseUri,
             @DefaultValue(DEFAULT_PING_PATH) String pingPath,
             @DefaultValue(DEFAULT_BASE_PATH) String basePath,
+            @DefaultValue(DEFAULT_SØKNAD_PATH) String søknadPath,
             @DefaultValue("true") boolean enabled) {
-        this.uri = uri;
-        this.basePath = basePath;
-        this.pingPath = pingPath;
         this.enabled = enabled;
-    }
-
-    String getBasePath() {
-        return basePath;
+        this.pdfUri = uri(baseUri, basePath + søknadPath);
+        this.pingUri = uri(baseUri, basePath + pingPath);
     }
 
     boolean isEnabled() {
         return enabled;
     }
 
-    String getPingPath() {
-        return pingPath;
-    }
-
-    URI getUri() {
-        return uri;
+    URI pdfUri() {
+        return pdfUri;
     }
 
     URI pingUri() {
-        return URIUtil.uri(getUri(), getPingPath());
+        return pingUri;
     }
 
     String name() {
-        return getUri().getHost();
+        return pdfUri.getHost();
     }
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " [pingPath=" + pingPath + ", enabled=" + enabled + ", uri=" + uri
-                + ", basePath=" + basePath
+        return getClass().getSimpleName() + " [pingUri=" + pingUri + ", enabled=" + enabled + ", pdfUri=" + pdfUri
                 + "]";
     }
+
 }
--- src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/pdftjeneste/PdfGeneratorConnection.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/pdftjeneste/PdfGeneratorConnection.java
@@ -1,19 +1,11 @@
 package no.nav.foreldrepenger.mottak.innsending.pdf.pdftjeneste;
 
-import static no.nav.foreldrepenger.mottak.innsending.pdf.pdftjeneste.PdfGeneratorConfig.SØKNAD;
-import static no.nav.foreldrepenger.mottak.util.URIUtil.uri;
-
 import java.net.URI;
-
-import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import no.nav.foreldrepenger.mottak.http.AbstractRestConnection;
 import no.nav.foreldrepenger.mottak.http.PingEndpointAware;
@@ -24,33 +16,17 @@ public class PdfGeneratorConnection extends AbstractRestConnection implements Pi
     private static final Logger LOG = LoggerFactory.getLogger(PdfGeneratorConnection.class);
     private final PdfGeneratorConfig config;
 
-    @Inject
-    private ObjectMapper mapper;
-
     public PdfGeneratorConnection(RestOperations operations, PdfGeneratorConfig config) {
         super(operations);
         this.config = config;
     }
 
     byte[] genererPdf(DokumentBestilling dto) {
-        logJSON(dto);
-        return postIfEnabled(uri(config.getUri(), SØKNAD), dto);
-    }
-
-    private byte[] postIfEnabled(URI uri, DokumentBestilling body) {
         if (config.isEnabled()) {
-            return postForObject(uri, body, byte[].class);
+            return postForObject(config.pdfUri(), dto, byte[].class);
         }
         LOG.info("PdfGenerator er ikke aktivert");
         return new byte[0];
-    }
-
-    private void logJSON(DokumentBestilling dto) {
-        try {
-            LOG.trace("JSON er {}", mapper.writerWithDefaultPrettyPrinter().writeValueAsString(dto));
-        } catch (JsonProcessingException e) {
-
-        }
     }
 
     @Override
