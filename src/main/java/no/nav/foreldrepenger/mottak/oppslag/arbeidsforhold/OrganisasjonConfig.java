25fdfad7a678f1b7cf15a183442a3f4cd89ed807
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-08-07 09:55:59 +0200

    historikk=true

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/OrganisasjonConfig.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/OrganisasjonConfig.java
@@ -14,6 +14,7 @@ import no.nav.foreldrepenger.mottak.oppslag.AbstractConfig;
 @ConfigurationProperties(prefix = "organisasjon")
 public class OrganisasjonConfig extends AbstractConfig {
 
+    static final String HISTORIKK = "historikk";
     private static final String V1_ORGANISASJON = "/v1/organisasjon/{orgnr}";
     private static final String NAV = "998004993";
     private final String organisasjonPath;
@@ -35,6 +36,7 @@ public class OrganisasjonConfig extends AbstractConfig {
 
     URI getOrganisasjonURI(UriBuilder b, String orgnr) {
         return b.path(organisasjonPath)
+                .queryParam(HISTORIKK, true)
                 .build(orgnr);
     }
 
