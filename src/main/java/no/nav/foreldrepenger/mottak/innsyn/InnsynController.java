74488a7adc45b7ebd043e349901335ca47b893d2
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-08-12 23:14:43 +0200

    meta annotation

--- src/main/java/no/nav/foreldrepenger/mottak/innsyn/InnsynController.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsyn/InnsynController.java
@@ -1,26 +1,20 @@
 package no.nav.foreldrepenger.mottak.innsyn;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import no.nav.foreldrepenger.mottak.domain.Fødselsnummer;
 import no.nav.foreldrepenger.mottak.domain.Sak;
+import no.nav.foreldrepenger.mottak.http.ProtectedRestController;
 import no.nav.foreldrepenger.mottak.innsyn.uttaksplan.Uttaksplan;
 import no.nav.foreldrepenger.mottak.innsyn.vedtak.Vedtak;
 import no.nav.foreldrepenger.mottak.oppslag.Oppslag;
 import no.nav.foreldrepenger.mottak.oppslag.arbeidsforhold.ArbeidsforholdTjeneste;
 import no.nav.foreldrepenger.mottak.oppslag.arbeidsforhold.EnkeltArbeidsforhold;
-import no.nav.security.token.support.core.api.ProtectedWithClaims;
 
-@RestController
-@RequestMapping(path = InnsynController.INNSYN, produces = APPLICATION_JSON_VALUE)
-@ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
+@ProtectedRestController(InnsynController.INNSYN)
 public class InnsynController {
 
     public static final String INNSYN = "/innsyn";
