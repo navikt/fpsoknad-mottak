4e3269e59dca05fbd3e8a56fa70443a4d74a81ad
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-08-26 11:41:29 +0200

    slå opp navn hvis ikke orgnr

--- src/main/java/no/nav/foreldrepenger/mottak/domain/Navn.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/Navn.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.mottak.domain;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.google.common.base.Joiner;
 
 import lombok.Data;
 import no.nav.foreldrepenger.mottak.domain.felles.Kjønn;
@@ -18,10 +19,14 @@ public class Navn {
 
     @JsonCreator
     public Navn(@JsonProperty("fornavn") String fornavn, @JsonProperty("mellomnavn") String mellomnavn,
-            @JsonProperty("etternavn") String etternavn,@JsonProperty("kjønn") Kjønn kjønn) {
+            @JsonProperty("etternavn") String etternavn, @JsonProperty("kjønn") Kjønn kjønn) {
         this.fornavn = fornavn;
         this.mellomnavn = mellomnavn;
         this.etternavn = etternavn;
         this.kjønn = kjønn;
+    }
+
+    public String navn() {
+        return Joiner.on(' ').skipNulls().join(fornavn, mellomnavn, etternavn);
     }
 }
--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/OrganisasjonConnection.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/OrganisasjonConnection.java
@@ -15,21 +15,32 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import no.nav.foreldrepenger.mottak.domain.Fødselsnummer;
+import no.nav.foreldrepenger.mottak.domain.Navn;
 import no.nav.foreldrepenger.mottak.http.AbstractWebClientConnection;
+import no.nav.foreldrepenger.mottak.oppslag.OppslagConnection;
 
 @Component
 public class OrganisasjonConnection extends AbstractWebClientConnection {
 
     private static final Logger LOG = LoggerFactory.getLogger(OrganisasjonConnection.class);
     private final OrganisasjonConfig cfg;
+    private OppslagConnection oppslag;
 
-    public OrganisasjonConnection(@Qualifier(ORGANISASJON) WebClient client, OrganisasjonConfig cfg) {
+    public OrganisasjonConnection(@Qualifier(ORGANISASJON) WebClient client, OppslagConnection oppslag, OrganisasjonConfig cfg) {
         super(client, cfg);
         this.cfg = cfg;
+        this.oppslag = oppslag;
     }
 
     @Cacheable(cacheNames = "organisasjon")
     public String navn(String orgnr) {
+        if (orgnr != null && orgnr.length() == 11) {
+            LOG.info("{} er et fødselsnummer", orgnr);
+            return Optional.ofNullable(oppslag.navn(Fødselsnummer.valueOf(orgnr)))
+                    .map(Navn::navn)
+                    .orElse("Privat arbeidsgiver");
+        }
         LOG.info("Henter organisasjonsnavn for {}", orgnr);
         try {
             var navn = Optional.ofNullable(getWebClient()
