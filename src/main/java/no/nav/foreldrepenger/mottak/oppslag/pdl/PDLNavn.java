9b756d739c7da87482eced7a06e8714415d7adbb
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-10-07 17:03:11 +0200

    revert

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLNavn.java
+++
@@ -1,7 +0,0 @@
-package no.nav.foreldrepenger.mottak.oppslag.pdl;
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public record PDLNavn(String fornavn, String mellomnavn, String etternavn) {
-}
