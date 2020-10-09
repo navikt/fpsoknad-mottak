13e4913a58f3403fd52b611a73b3c0e31962b3ed
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-10-09 14:17:00 +0200

    kontonr fra tps

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLConfig.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLConfig.java
@@ -1,5 +1,7 @@
 package no.nav.foreldrepenger.mottak.oppslag.pdl;
 
+import java.net.URI;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
@@ -10,12 +12,19 @@ import no.nav.foreldrepenger.mottak.oppslag.AbstractConfig;
 public class PDLConfig extends AbstractConfig {
     private static final String DEFAULT_BASE_URI = "http://fpsoknad-oppslag/api";
     private static final String DEFAULT_PING_PATH = "actuator/health/liveness";
+    private static final String DEFAULT_KONTONR_URI = "http://fpsoknad-oppslag/api/person/kontonr";
+    private final URI kontonummerURI;
 
     @ConstructorBinding
     public PDLConfig(@DefaultValue(DEFAULT_PING_PATH) String pingPath,
             @DefaultValue("true") boolean enabled,
-            @DefaultValue(DEFAULT_BASE_URI) String baseUri) {
+            @DefaultValue(DEFAULT_BASE_URI) String baseUri, @DefaultValue(DEFAULT_KONTONR_URI) URI kontonummerURI) {
         super(baseUri, pingPath, enabled);
+        this.kontonummerURI = kontonummerURI;
+    }
+
+    public URI getKontonummerURI() {
+        return kontonummerURI;
     }
 
     @Override
--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLConnection.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLConnection.java
@@ -5,27 +5,40 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestOperations;
 
 import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
+import no.nav.foreldrepenger.mottak.http.AbstractRestConnection;
 import no.nav.foreldrepenger.mottak.util.TokenUtil;
 
 @Component
-public class PDLConnection {
+public class PDLConnection extends AbstractRestConnection {
 
     private static final Logger LOG = LoggerFactory.getLogger(PDLConnection.class);
     private final GraphQLWebClient client;
     private final TokenUtil tokenUtil;
+    private PDLConfig cfg;
 
-    public PDLConnection(GraphQLWebClient client, TokenUtil tokenUtil) {
+    public PDLConnection(GraphQLWebClient client, RestOperations restOperations, PDLConfig cfg, TokenUtil tokenUtil) {
+        super(restOperations);
         this.client = client;
         this.tokenUtil = tokenUtil;
+        this.cfg = cfg;
     }
 
     public PDLPerson hentPerson() {
         LOG.info("PDL Henter person");
         var p = client.post("query-person.graphql", Map.of("ident", tokenUtil.getSubject()), PDLPerson.class).block();
         LOG.info("PDL person {}", p);
+        var kontonr = kontonr();
+        LOG.info("PDL kontonummer {}", kontonr);
+
         return p;
+    }
+
+    private String kontonr() {
+        LOG.info("PDL Henter kontonummer fra  {}", cfg.getKontonummerURI());
+        return getForObject(cfg.getKontonummerURI(), String.class);
     }
 
     @Override
