d05c21ba03fed4ee8fb92d2592739aba9f360df3
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-10-20 20:44:56 +0200

    no record

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLDødsfall.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLDødsfall.java
@@ -1,12 +1,18 @@
 package no.nav.foreldrepenger.mottak.oppslag.pdl;
 
-import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
-
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonAutoDetect(fieldVisibility = ANY)
-public record PDLDødsfall(LocalDate dødsdato) {
+import lombok.Data;
 
+@Data
+public class PDLDødsfall {
+    private final LocalDate dødsdato;
+
+    @JsonCreator
+    public PDLDødsfall(@JsonProperty("doedsdato") LocalDate dødsdato) {
+        this.dødsdato = dødsdato;
+    }
 }
