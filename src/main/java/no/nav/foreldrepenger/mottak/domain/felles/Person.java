09eaa4ca696151b3e3cbdc4717224ea02da4ef98
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-10-22 12:28:25 +0200

    bruk Navn

--- src/main/java/no/nav/foreldrepenger/mottak/domain/felles/Person.java
+++ src/main/java/no/nav/foreldrepenger/mottak/domain/felles/Person.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.mottak.domain.felles;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -13,16 +14,14 @@ import com.neovisionaries.i18n.CountryCode;
 import lombok.Data;
 import no.nav.foreldrepenger.mottak.domain.AktørId;
 import no.nav.foreldrepenger.mottak.domain.Fødselsnummer;
+import no.nav.foreldrepenger.mottak.domain.Navn;
 
 @JsonInclude(NON_NULL)
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Person {
     private final Fødselsnummer fnr;
-    private final String fornavn;
-    private final String mellomnavn;
-    private final String etternavn;
-    private final Kjønn kjønn;
+    private final Navn navn;
     private final LocalDate fødselsdato;
     private final String målform;
     private final CountryCode land;
@@ -32,24 +31,35 @@ public class Person {
 
     @JsonCreator
     public Person(@JsonProperty("fnr") Fødselsnummer fnr,
-            @JsonProperty("fornavn") String fornavn,
-            @JsonProperty("mellomnavn") String mellomnavn,
-            @JsonProperty("etternavn") String etternavn,
-            @JsonProperty("kjønn") Kjønn kjønn,
+            @JsonProperty("navn") Navn navn,
             @JsonProperty("fødselsdato") LocalDate fødselsdato,
             @JsonProperty("målform") String målform,
             @JsonProperty("land") CountryCode land,
             @JsonProperty("ikkeNordiskEøsLand") Boolean ikkeNordiskEøsLand,
             @JsonProperty("bankkonto") Bankkonto bankkonto) {
         this.fnr = fnr;
-        this.fornavn = fornavn;
-        this.mellomnavn = mellomnavn;
-        this.etternavn = etternavn;
-        this.kjønn = kjønn;
+        this.navn = navn;
         this.fødselsdato = fødselsdato;
         this.målform = målform;
         this.land = land;
         this.ikkeNordiskEøsLand = ikkeNordiskEøsLand;
         this.bankkonto = bankkonto;
+    }
+
+    public String getFornavn() {
+        return Optional.ofNullable(navn).map(Navn::getFornavn).orElse(null);
+    }
+
+    public String getMellomnavn() {
+        return Optional.ofNullable(navn).map(Navn::getMellomnavn).orElse(null);
+    }
+
+    public String getEtternavn() {
+        return Optional.ofNullable(navn).map(Navn::getEtternavn).orElse(null);
+    }
+
+    public Kjønn getKjønn() {
+        return Optional.ofNullable(navn).map(Navn::getKjønn).orElse(Kjønn.U);
+
     }
 }
