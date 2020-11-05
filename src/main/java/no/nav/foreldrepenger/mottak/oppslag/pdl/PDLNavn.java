2e4606d59b8d6a5afdaf289a8ee61a328b7c2828
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-11-05 13:07:24 +0100

    dev controller

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLNavn.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/pdl/PDLNavn.java
@@ -3,12 +3,13 @@ package no.nav.foreldrepenger.mottak.oppslag.pdl;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonAutoDetect(fieldVisibility = ANY)
 record PDLNavn(String fornavn, String mellomnavn, String etternavn) {
 }
 
 @JsonAutoDetect(fieldVisibility = ANY)
-record PDLWrappedNavn(PDLNavn navn) {
+record PDLWrappedNavn(@JsonProperty("navn") PDLNavn navn) {
 
 }
