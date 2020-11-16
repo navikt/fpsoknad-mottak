c4ba3d01ed8c0b12f1cd2c2d833af4a43aafb836
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-11-16 10:15:31 +0100

    refresh ved konstruksjon

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/sts/STSSystemTokenTjeneste.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/sts/STSSystemTokenTjeneste.java
@@ -2,10 +2,7 @@ package no.nav.foreldrepenger.mottak.oppslag.sts;
 
 import org.springframework.stereotype.Service;
 
-import no.nav.foreldrepenger.boot.conditionals.ConditionalOnK8s;
-
 @Service
-@ConditionalOnK8s
 public class STSSystemTokenTjeneste implements SystemTokenTjeneste {
     private final STSConnection connection;
     private SystemToken systemToken;
