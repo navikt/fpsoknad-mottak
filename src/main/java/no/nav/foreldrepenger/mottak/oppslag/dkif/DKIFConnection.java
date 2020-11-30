7df0f961593b9aeb9bae391bda608648aa4309f0
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-11-30 16:22:34 +0100

    bomb out on error

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/dkif/DKIFConnection.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/dkif/DKIFConnection.java
@@ -7,7 +7,9 @@ import static org.springframework.util.StringUtils.capitalize;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import no.nav.foreldrepenger.mottak.http.AbstractWebClientConnection;
@@ -32,6 +34,7 @@ public class DKIFConnection extends AbstractWebClientConnection {
                 .uri(b -> cfg.kontaktUri(b))
                 .accept(APPLICATION_JSON)
                 .retrieve()
+                .onStatus(HttpStatus::isError, ClientResponse::createException)
                 .toEntityList(DigitalKontaktinfo.class)
                 .block()
                 .getBody()
