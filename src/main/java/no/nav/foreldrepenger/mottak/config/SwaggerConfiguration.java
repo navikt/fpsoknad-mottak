d5504a8d02d7b035ebb293f0b5f548a62e1e9325
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-12-14 18:24:15 +0100

    security seagger

--- src/main/java/no/nav/foreldrepenger/mottak/config/SwaggerConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/SwaggerConfiguration.java
@@ -8,11 +8,11 @@ import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.OpenIdConnectSchemeBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.HttpAuthenticationScheme;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -34,11 +34,7 @@ public class SwaggerConfiguration {
     }
 
     private static List<SecurityScheme> apiKeys() {
-        return List.of(new OpenIdConnectSchemeBuilder()
-                .name("name")
-                .openIdConnectUrl(
-                        "https://navtestb2c.b2clogin.com/navtestb2c.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_idporten_ver1")
-                .build());
+        return List.of(HttpAuthenticationScheme.JWT_BEARER_BUILDER.build());
     }
 
     private static List<SecurityContext> securityContexts() {
