6909ccfac911d06f71ba8b10e81de4af510dcc7d
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-03-20 22:40:23 +0100

    lek med naming strategy

--- src/main/java/no/nav/foreldrepenger/mottak/tokendings/TokendingsResponse.java
+++ src/main/java/no/nav/foreldrepenger/mottak/tokendings/TokendingsResponse.java
@@ -1,10 +1,12 @@
 package no.nav.foreldrepenger.mottak.tokendings;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record TokendingsResponse(
-        @JsonProperty("access_token") String accessToken,
-        @JsonProperty("issued_token_type") String issuedTokenType,
-        @JsonProperty("token_type") String tokenType,
-        @JsonProperty("expires_in") String expiresIn) {
+        String accessToken,
+        String issuedTokenType,
+        String tokenType,
+        String expiresIn) {
 }
