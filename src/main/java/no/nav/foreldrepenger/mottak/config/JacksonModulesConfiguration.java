3aa36be9209933c6ac727c8d8dac75eb0a732d13
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-03-22 18:57:31 +0100

    add mixin to OAuth2AccessTokenResponse

--- src/main/java/no/nav/foreldrepenger/mottak/config/JacksonModulesConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/JacksonModulesConfiguration.java
@@ -1,9 +1,13 @@
 package no.nav.foreldrepenger.mottak.config;
 
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.Module;
+
+import no.nav.foreldrepenger.mottak.http.interceptors.TokenExchangeResponseMixin;
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse;
 
 @Configuration
 class JacksonModulesConfiguration {
@@ -13,4 +17,8 @@ class JacksonModulesConfiguration {
         return new CustomSerializerModule();
     }
 
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return b -> b.mixIn(OAuth2AccessTokenResponse.class, TokenExchangeResponseMixin.class);
+    }
 }
