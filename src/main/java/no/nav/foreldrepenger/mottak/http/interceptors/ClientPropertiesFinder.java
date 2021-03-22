636de85e3d8cb4368b5503bbd29f775bc70691d8
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-03-22 21:06:58 +0100

    finder

--- src/main/java/no/nav/foreldrepenger/mottak/http/interceptors/ClientPropertiesFinder.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/interceptors/ClientPropertiesFinder.java
@@ -2,9 +2,12 @@ package no.nav.foreldrepenger.mottak.http.interceptors;
 
 import java.net.URI;
 
+import org.springframework.stereotype.Component;
+
 import no.nav.security.token.support.client.core.ClientProperties;
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties;
 
+@Component
 public interface ClientPropertiesFinder {
 
     default ClientProperties findProperties(ClientConfigurationProperties configs, URI uri) {
