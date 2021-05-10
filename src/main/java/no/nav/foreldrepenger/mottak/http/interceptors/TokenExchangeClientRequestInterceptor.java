0b8d5b6e342d8059d2335fa9fdccb04610033591
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-10 14:56:46 +0200

    component

--- src/main/java/no/nav/foreldrepenger/mottak/http/interceptors/TokenExchangeClientRequestInterceptor.java
+++ src/main/java/no/nav/foreldrepenger/mottak/http/interceptors/TokenExchangeClientRequestInterceptor.java
@@ -12,11 +12,13 @@ import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
 
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService;
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties;
 
 //@ConditionalOnK8s
+@Component
 @Order(HIGHEST_PRECEDENCE)
 public class TokenExchangeClientRequestInterceptor implements ClientHttpRequestInterceptor {
 
