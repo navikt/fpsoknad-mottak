5a608527de8f26d1e4896555229ad0f71900b0e3
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-12 13:05:39 +0200

    pdf stub if not

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/pdftjeneste/PdfGeneratorStub.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/pdftjeneste/PdfGeneratorStub.java
@@ -1,17 +1,13 @@
 package no.nav.foreldrepenger.mottak.innsending.pdf.pdftjeneste;
 
-import static no.nav.foreldrepenger.boot.conditionals.Cluster.LOCAL;
-import static no.nav.foreldrepenger.boot.conditionals.Cluster.TEST;
-import static no.nav.foreldrepenger.boot.conditionals.Cluster.VTP;
-
 import org.apache.commons.lang3.RandomUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 
-import no.nav.foreldrepenger.boot.conditionals.ConditionalOnClusters;
 import no.nav.foreldrepenger.mottak.innsending.pdf.modell.DokumentBestilling;
 
 @Service
-@ConditionalOnClusters(clusters = { TEST, LOCAL, VTP })
+@ConditionalOnMissingBean(PdfGeneratorTjeneste.class)
 public class PdfGeneratorStub implements PdfGenerator {
 
     @Override
