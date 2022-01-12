630a48793702991f8a6e25014a832f51e1c64fcb
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2022-01-12 13:08:17 +0100

    IgnoreUnknownProperties midlertidig på Frilans

--- src/main/java/no/nav/foreldrepenger/mottak/config/JacksonConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/JacksonConfiguration.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.mottak.config;
 
+import no.nav.foreldrepenger.common.domain.felles.opptjening.Frilans;
 import org.springframework.boot.jackson.JsonComponentModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,8 @@ public class JacksonConfiguration {
     public ObjectMapper customObjectmapper() {
         return DefaultJsonMapper.MAPPER
             .registerModule(new JsonComponentModule()) // Spring Bean and Jackson Module to register @JsonComponent annotated beans
-            .addMixIn(OAuth2AccessTokenResponse.class, IgnoreUnknownMixin.class);
+            .addMixIn(OAuth2AccessTokenResponse.class, IgnoreUnknownMixin.class)
+            .addMixIn(Frilans.class, IgnoreUnknownMixin.class);
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
