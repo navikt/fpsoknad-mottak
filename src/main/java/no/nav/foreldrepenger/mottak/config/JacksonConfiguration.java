56e79a272a0f99b314c243d6907ef4c7dd3490ae
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2022-02-21 15:13:52 +0100

    Revert "Revert "Revert "Validering (#1610)" (#1621)" (#1622)"
    This reverts commit abde1773ca5055f5b992cde5bc9982871300a12c.

--- src/main/java/no/nav/foreldrepenger/mottak/config/JacksonConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/JacksonConfiguration.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import no.nav.foreldrepenger.common.mapper.DefaultJsonMapper;
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse;
 
 @Configuration
 public class JacksonConfiguration {
@@ -16,6 +17,11 @@ public class JacksonConfiguration {
     @Primary
     public ObjectMapper customObjectmapper() {
         return DefaultJsonMapper.MAPPER
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            .addMixIn(OAuth2AccessTokenResponse.class, IgnoreUnknownMixin.class);
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private interface IgnoreUnknownMixin {
+
     }
 }
