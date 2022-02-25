5a354a5f81eb9e240f433eb1e85c664b2e4ad234
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2022-02-25 15:36:59 +0100

    Bumper spring-boot-starter-parent 2.6.4 og erstatter Springfox med SpringDocs (#1641)
    * Bumper spring-boot-starter-parent 2.6.4 og erstatter Springfox med SpringDocs
    Fjerner disabled test og avhengigheten til mock-oauth2-server

--- src/main/java/no/nav/foreldrepenger/mottak/config/SwaggerConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/SwaggerConfiguration.java
@@ -1,28 +1,21 @@
 package no.nav.foreldrepenger.mottak.config;
 
-import static springfox.documentation.spi.DocumentationType.OAS_30;
-
-import java.util.Set;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.spring.web.plugins.Docket;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
-@EnableOpenApi
 public class SwaggerConfiguration {
-    @Bean
-    public Docket productApi() {
-        return new Docket(OAS_30)
-                .protocols(Set.of("http", "https"))
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 
+    @Bean
+    public OpenAPI swaggerOpenAPI() {
+        return new OpenAPI()
+            .info(new Info().title("Fpsoknad-mottak")
+                .description("Mottar søknader om svangerskapspenger, foreldrepenger og engangsstønad fra frontend og sender dem videre inn i NAV for behandling")
+                .version("v0.0.1")
+                .license(new License().name("MIT").url("http://nav.no")));
+    }
 }
