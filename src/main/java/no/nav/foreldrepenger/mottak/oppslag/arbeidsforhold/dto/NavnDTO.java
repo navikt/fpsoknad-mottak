e44edc922cd8c22e82b95bc7f6fb1577080ba938
Espen Waaga <espen.waaga@nav.no>
2022-03-18 16:06:25 +0100

    Revert "Revert "Rydder i klient mot eereg og mapper respons til DTO (#1710)""
    This reverts commit 1a9cc894ee4ec4bc5ede1400c4a3abce5afa5839.

---
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/dto/NavnDTO.java
@@ -0,0 +1,9 @@
+package no.nav.foreldrepenger.mottak.oppslag.arbeidsforhold.dto;
+
+public record NavnDTO(String navnelinje1,
+                      String navnelinje2,
+                      String navnelinje3,
+                      String navnelinje4,
+                      String navnelinje5) {
+
+}
