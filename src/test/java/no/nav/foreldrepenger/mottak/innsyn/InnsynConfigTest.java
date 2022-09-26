60121b9e0edcbd0ba8c6e1041c9b6c8deff3f535
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2022-09-26 08:09:42 +0200

    Fix: Innsynslink blir laget fra Location header fra respons. (#1910)
    * Fix: Innsynslink blir laget fra Location header fra respons.

---
+++ src/test/java/no/nav/foreldrepenger/mottak/innsyn/InnsynConfigTest.java
@@ -0,0 +1,28 @@
+package no.nav.foreldrepenger.mottak.innsyn;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+
+class InnsynConfigTest {
+
+
+    @Test
+    void verifiserKorrektLinkVedServiceDiscovery() {
+        URI baseUri = URI.create("http://fpinfo/fpinfo");
+        InnsynConfig innsynConfig = new InnsynConfig(null, true, baseUri, null);
+        URI link = innsynConfig.createLink("/fpinfo/api/dokumentforsendelse/behandling?behandlingId=1733259");
+        assertThat(link).isEqualTo(URI.create("http://fpinfo/fpinfo/api/dokumentforsendelse/behandling?behandlingId=1733259"));
+    }
+
+    @Test
+    void verifiserKorrektLinkNårDetIkkeErServiceDiscovery() {
+        URI baseUri = URI.create("http://fpinfo:8080/fpinfo");
+        InnsynConfig innsynConfig = new InnsynConfig(null, true, baseUri, null);
+        URI link = innsynConfig.createLink("/fpinfo/api/dokumentforsendelse/behandling?behandlingId=1733259");
+        assertThat(link).isEqualTo(URI.create("http://fpinfo:8080/fpinfo/api/dokumentforsendelse/behandling?behandlingId=1733259"));
+    }
+}
