5697c56863345fecd8e87c78b5340605011a4606
Anders Palfi <anderspalfi@gmail.com>
2022-10-10 10:44:41 +0200

    Fjerner ubrukt(?) tjeneste for oppslag

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/OppslagController.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/OppslagController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import no.nav.foreldrepenger.common.domain.AktørId;
-import no.nav.foreldrepenger.common.domain.Fødselsnummer;
 import no.nav.foreldrepenger.common.domain.Navn;
 import no.nav.foreldrepenger.common.domain.felles.Person;
 import no.nav.foreldrepenger.common.util.TokenUtil;
@@ -30,34 +29,20 @@ public class OppslagController {
         this.tokenUtil = tokenUtil;
     }
 
-    @GetMapping("/aktoer")
-    public AktørId aktør() {
-        LOG.info("Tjeneste kalles aktoer");
-        return oppslag.aktørId(tokenUtil.autentisertBrukerOrElseThrowException());
-    }
-
     @GetMapping("/ping")
     @Unprotected
     public String ping() {
         return oppslag.ping();
     }
 
-    @GetMapping("/fnr")
-    public Fødselsnummer fnr(@Valid @RequestParam(name = "aktorId") AktørId aktorId) {
-        LOG.info("Tjeneste kalles fnr");
-        return oppslag.fnr(aktorId);
+    @GetMapping("/aktoer")
+    public AktørId aktør() {
+        return oppslag.aktørId(tokenUtil.autentisertBrukerOrElseThrowException());
     }
 
     @GetMapping("/navn")
     public Navn navn(@Valid @RequestParam(name = "aktorId") AktørId aktorId) {
-        LOG.info("Tjeneste kalles navn");
         return oppslag.navn(aktorId.value());
-    }
-
-    @GetMapping("/navnfnr")
-    public Navn navnfnr(@Valid @RequestParam(name = "fnr") Fødselsnummer fnr) {
-        LOG.info("Tjeneste kalles navnfnr");
-        return oppslag.navn(fnr.value());
     }
 
     @GetMapping("/person")
