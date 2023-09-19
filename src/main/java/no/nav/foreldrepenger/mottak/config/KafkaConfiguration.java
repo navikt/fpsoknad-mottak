bdeb0a1f448ae636dffe2f069abb4066cf0efaac
Steffen Pøhner <steffen.h.pohner@nav.no>
2023-09-19 14:19:15 +0200

    Må lage KafkaTemplate selv

--- src/main/java/no/nav/foreldrepenger/mottak/config/KafkaConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/KafkaConfiguration.java
@@ -2,20 +2,21 @@ package no.nav.foreldrepenger.mottak.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.support.KafkaUtils;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.LoggingProducerListener;
-
-import static no.nav.foreldrepenger.common.util.Constants.NAV_CALL_ID;
 
 @Configuration
 public class KafkaConfiguration {
 
     @Bean
-    public LoggingProducerListener<String, String> loggingProducerListener() {
-        LoggingProducerListener<String, String> loggingProducerListener = new LoggingProducerListener<>();
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        var loggingProducerListener = new LoggingProducerListener<String, String>();
         loggingProducerListener.setIncludeContents(false);
-        KafkaUtils.setProducerRecordFormatter(r -> String.format("Feil mot topic %s for callId %s" + r.topic() + r.headers().headers(NAV_CALL_ID)));
-        return loggingProducerListener;
+
+        var kafkaTemplate = new KafkaTemplate<>(producerFactory);
+        kafkaTemplate.setProducerListener(loggingProducerListener);
+        return kafkaTemplate;
     }
 
 }
