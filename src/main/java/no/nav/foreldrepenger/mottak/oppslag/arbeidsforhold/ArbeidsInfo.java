1c77465696af0efcd2049e33d49817b9fb59ff93
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-09-28 14:41:56 +0200

    Sender ikke innsendingshendelse lenger og fjerner endepunkt som info-… (#2167)
    Sender ikke innsendingshendelse lenger og fjerner endepunkt som info-historikk brukte

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/ArbeidsInfo.java
+++ src/main/java/no/nav/foreldrepenger/mottak/oppslag/arbeidsforhold/ArbeidsInfo.java
@@ -2,12 +2,8 @@ package no.nav.foreldrepenger.mottak.oppslag.arbeidsforhold;
 
 import java.util.List;
 
-import no.nav.foreldrepenger.common.domain.Orgnummer;
-
 public interface ArbeidsInfo {
 
     List<EnkeltArbeidsforhold> hentArbeidsforhold();
-
-    String orgnavn(Orgnummer orgnr);
 
 }
