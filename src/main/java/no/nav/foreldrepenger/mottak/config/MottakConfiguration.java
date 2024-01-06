e246b8ca736f380737ce9ab48c5ed50caf238982
Steffen Pøhner <steffen.h.pohner@nav.no>
2024-01-06 10:32:42 +0100

    Logg manglende tekstnøkler i messagesources

--- src/main/java/no/nav/foreldrepenger/mottak/config/MottakConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/mottak/config/MottakConfiguration.java
@@ -1,10 +1,14 @@
 package no.nav.foreldrepenger.mottak.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.util.Locale;
 
 @Configuration
 public class MottakConfiguration {
@@ -15,7 +19,7 @@ public class MottakConfiguration {
     @Bean
     @Qualifier(LANDKODER)
     public MessageSource landkoderSource() {
-        var messageSource = new ResourceBundleMessageSource();
+        var messageSource = new LoggingMessageSource();
         messageSource.setBasename(LANDKODER);
         return messageSource;
     }
@@ -23,8 +27,21 @@ public class MottakConfiguration {
     @Bean
     @Qualifier(KVITTERINGSTEKSTER)
     public MessageSource kvitteringsteksterSource() {
-        var messageSource = new ResourceBundleMessageSource();
+        var messageSource = new LoggingMessageSource();
         messageSource.setBasename(KVITTERINGSTEKSTER);
         return messageSource;
+    }
+
+    static class LoggingMessageSource extends ResourceBundleMessageSource {
+        private static final Logger LOG = LoggerFactory.getLogger(LoggingMessageSource.class);
+        @Override
+        protected String getMessageInternal(String code, Object[] args, Locale locale) {
+            var message = super.getMessageInternal(code, args, locale);
+            if (message == null) {
+                var basename = String.join(", ", super.getBasenameSet());
+                LOG.warn("Finner ikke nøkkel '{}' i messagebundle '{}' for locale '{}'", code, basename, locale);
+            }
+            return message;
+        }
     }
 }
