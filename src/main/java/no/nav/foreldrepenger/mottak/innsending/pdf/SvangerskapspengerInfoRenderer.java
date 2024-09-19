b8fa2dbddaa7835ec1fe01b9a87aa5a46ea716c0
Steffen Pøhner <steffen.h.pohner@nav.no>
2024-09-19 10:46:22 +0200

    Bump fpsoknad-felles (svp ferieperioder). Legg til skjelett for pdf-endringer. TFP-5815

--- src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/SvangerskapspengerInfoRenderer.java
+++ src/main/java/no/nav/foreldrepenger/mottak/innsending/pdf/SvangerskapspengerInfoRenderer.java
@@ -1,6 +1,10 @@
 package no.nav.foreldrepenger.mottak.innsending.pdf;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
+
+import no.nav.foreldrepenger.common.domain.svangerskapspenger.AvtaltFerie;
 
 @Component
 public class SvangerskapspengerInfoRenderer extends FellesSøknadInfoRenderer {
@@ -8,4 +12,10 @@ public class SvangerskapspengerInfoRenderer extends FellesSøknadInfoRenderer {
     public SvangerskapspengerInfoRenderer(PdfElementRenderer renderer, SøknadTextFormatter textFormatter) {
         super(renderer, textFormatter);
     }
+
+    protected float feriePerioder(List<AvtaltFerie> avtaltFerie, FontAwareCos cos, float y) {
+        // placeholder for avtaltFerie
+        return y;
+    }
+
 }
