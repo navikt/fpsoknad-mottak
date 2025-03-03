2ec888f1496f9b0b98aa0b3d4a629e1417ef3ddc
espenwaaga <espen.waaga@nav.no>
2025-03-03 10:57:27 +0100

    Fjern gammel dto

--- src/main/java/no/nav/foreldrepenger/mottak/oppslag/dkif/Kontaktinformasjon.java
+++
@@ -1,8 +0,0 @@
-package no.nav.foreldrepenger.mottak.oppslag.dkif;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import no.nav.foreldrepenger.common.oppslag.dkif.Målform;
-
-public record Kontaktinformasjon(@JsonProperty("spraak") Målform målform) {
-}
